c7791f005b55604be4f8dbc9362057342e4b452c
Kevin Seitz <kseitz10@gmail.com>
2013-05-21 18:04:55 -0500

    Restoring older menu design

--- FrEee.WinForms/Forms/MainMenuForm.Designer.cs
+++ FrEee.WinForms/Forms/MainMenuForm.Designer.cs
@@ -28,7 +28,9 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.tblButtonPanel = new System.Windows.Forms.TableLayoutPanel();
             this.btnQuit = new FrEee.WinForms.Controls.GameButton();
             this.gameButton7 = new FrEee.WinForms.Controls.GameButton();
             this.gameButton6 = new FrEee.WinForms.Controls.GameButton();
@@ -38,20 +40,72 @@
             this.gameButton2 = new FrEee.WinForms.Controls.GameButton();
             this.btnQuickStart = new FrEee.WinForms.Controls.GameButton();
             this.progressBar1 = new System.Windows.Forms.ProgressBar();
-            this.tblButtonPanel = new System.Windows.Forms.TableLayoutPanel();
-            this.lblStatus = new System.Windows.Forms.Label();
+            this.tableLayoutPanel1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.tblButtonPanel.SuspendLayout();
             this.SuspendLayout();
             // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tableLayoutPanel1.ColumnCount = 2;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 413F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.Controls.Add(this.pictureBox1, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.tblButtonPanel, 1, 0);
+            this.tableLayoutPanel1.Controls.Add(this.progressBar1, 0, 1);
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Margin = new System.Windows.Forms.Padding(0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 2;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 35F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(577, 403);
+            this.tableLayoutPanel1.TabIndex = 0;
+            // 
             // pictureBox1
             // 
-            this.pictureBox1.Anchor = System.Windows.Forms.AnchorStyles.None;
-            this.pictureBox1.Location = new System.Drawing.Point(90, 17);
+            this.pictureBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.pictureBox1.Location = new System.Drawing.Point(3, 3);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(407, 352);
+            this.pictureBox1.Size = new System.Drawing.Size(407, 362);
             this.pictureBox1.TabIndex = 2;
             this.pictureBox1.TabStop = false;
+            // 
+            // tblButtonPanel
+            // 
+            this.tblButtonPanel.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tblButtonPanel.ColumnCount = 1;
+            this.tblButtonPanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tblButtonPanel.Controls.Add(this.btnQuit, 0, 8);
+            this.tblButtonPanel.Controls.Add(this.gameButton7, 0, 7);
+            this.tblButtonPanel.Controls.Add(this.gameButton6, 0, 5);
+            this.tblButtonPanel.Controls.Add(this.gameButton5, 0, 4);
+            this.tblButtonPanel.Controls.Add(this.gameButton4, 0, 3);
+            this.tblButtonPanel.Controls.Add(this.gameButton3, 0, 2);
+            this.tblButtonPanel.Controls.Add(this.gameButton2, 0, 1);
+            this.tblButtonPanel.Controls.Add(this.btnQuickStart, 0, 0);
+            this.tblButtonPanel.Location = new System.Drawing.Point(416, 3);
+            this.tblButtonPanel.Name = "tblButtonPanel";
+            this.tblButtonPanel.RowCount = 9;
+            this.tblButtonPanel.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.tblButtonPanel.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.tblButtonPanel.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.tblButtonPanel.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.tblButtonPanel.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.tblButtonPanel.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.tblButtonPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tblButtonPanel.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.tblButtonPanel.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.tblButtonPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tblButtonPanel.Size = new System.Drawing.Size(158, 362);
+            this.tblButtonPanel.TabIndex = 3;
             // 
             // btnQuit
             // 
@@ -59,11 +113,11 @@
             | System.Windows.Forms.AnchorStyles.Right)));
             this.btnQuit.BackColor = System.Drawing.Color.Black;
             this.btnQuit.ForeColor = System.Drawing.Color.CornflowerBlue;
-            this.btnQuit.Location = new System.Drawing.Point(329, 47);
+            this.btnQuit.Location = new System.Drawing.Point(5, 323);
             this.btnQuit.Margin = new System.Windows.Forms.Padding(5, 5, 5, 4);
             this.btnQuit.Name = "btnQuit";
             this.btnQuit.Padding = new System.Windows.Forms.Padding(4);
-            this.btnQuit.Size = new System.Drawing.Size(100, 34);
+            this.btnQuit.Size = new System.Drawing.Size(148, 35);
             this.btnQuit.TabIndex = 7;
             this.btnQuit.Text = "Quit Game";
             this.btnQuit.UseVisualStyleBackColor = false;
@@ -75,11 +129,11 @@
             | System.Windows.Forms.AnchorStyles.Right)));
             this.gameButton7.BackColor = System.Drawing.Color.Black;
             this.gameButton7.ForeColor = System.Drawing.Color.CornflowerBlue;
-            this.gameButton7.Location = new System.Drawing.Point(329, 5);
+            this.gameButton7.Location = new System.Drawing.Point(5, 279);
             this.gameButton7.Margin = new System.Windows.Forms.Padding(5, 5, 5, 4);
             this.gameButton7.Name = "gameButton7";
             this.gameButton7.Padding = new System.Windows.Forms.Padding(4);
-            this.gameButton7.Size = new System.Drawing.Size(100, 33);
+            this.gameButton7.Size = new System.Drawing.Size(148, 35);
             this.gameButton7.TabIndex = 6;
             this.gameButton7.Text = "Credits";
             this.gameButton7.UseVisualStyleBackColor = false;
@@ -90,11 +144,11 @@
             | System.Windows.Forms.AnchorStyles.Right)));
             this.gameButton6.BackColor = System.Drawing.Color.Black;
             this.gameButton6.ForeColor = System.Drawing.Color.CornflowerBlue;
-            this.gameButton6.Location = new System.Drawing.Point(221, 47);
+            this.gameButton6.Location = new System.Drawing.Point(5, 225);
             this.gameButton6.Margin = new System.Windows.Forms.Padding(5, 5, 5, 4);
             this.gameButton6.Name = "gameButton6";
             this.gameButton6.Padding = new System.Windows.Forms.Padding(4);
-            this.gameButton6.Size = new System.Drawing.Size(98, 34);
+            this.gameButton6.Size = new System.Drawing.Size(148, 35);
             this.gameButton6.TabIndex = 5;
             this.gameButton6.Text = "Scenario";
             this.gameButton6.UseVisualStyleBackColor = false;
@@ -105,11 +159,11 @@
             | System.Windows.Forms.AnchorStyles.Right)));
             this.gameButton5.BackColor = System.Drawing.Color.Black;
             this.gameButton5.ForeColor = System.Drawing.Color.CornflowerBlue;
-            this.gameButton5.Location = new System.Drawing.Point(221, 5);
+            this.gameButton5.Location = new System.Drawing.Point(5, 181);
             this.gameButton5.Margin = new System.Windows.Forms.Padding(5, 5, 5, 4);
             this.gameButton5.Name = "gameButton5";
             this.gameButton5.Padding = new System.Windows.Forms.Padding(4);
-            this.gameButton5.Size = new System.Drawing.Size(98, 33);
+            this.gameButton5.Size = new System.Drawing.Size(148, 35);
             this.gameButton5.TabIndex = 4;
             this.gameButton5.Text = "Tutorial";
             this.gameButton5.UseVisualStyleBackColor = false;
@@ -120,11 +174,11 @@
             | System.Windows.Forms.AnchorStyles.Right)));
             this.gameButton4.BackColor = System.Drawing.Color.Black;
             this.gameButton4.ForeColor = System.Drawing.Color.CornflowerBlue;
-            this.gameButton4.Location = new System.Drawing.Point(113, 47);
+            this.gameButton4.Location = new System.Drawing.Point(5, 137);
             this.gameButton4.Margin = new System.Windows.Forms.Padding(5, 5, 5, 4);
             this.gameButton4.Name = "gameButton4";
             this.gameButton4.Padding = new System.Windows.Forms.Padding(4);
-            this.gameButton4.Size = new System.Drawing.Size(98, 34);
+            this.gameButton4.Size = new System.Drawing.Size(148, 35);
             this.gameButton4.TabIndex = 3;
             this.gameButton4.Text = "Load Game";
             this.gameButton4.UseVisualStyleBackColor = false;
@@ -135,11 +189,11 @@
             | System.Windows.Forms.AnchorStyles.Right)));
             this.gameButton3.BackColor = System.Drawing.Color.Black;
             this.gameButton3.ForeColor = System.Drawing.Color.CornflowerBlue;
-            this.gameButton3.Location = new System.Drawing.Point(113, 5);
+            this.gameButton3.Location = new System.Drawing.Point(5, 93);
             this.gameButton3.Margin = new System.Windows.Forms.Padding(5, 5, 5, 4);
             this.gameButton3.Name = "gameButton3";
             this.gameButton3.Padding = new System.Windows.Forms.Padding(4);
-            this.gameButton3.Size = new System.Drawing.Size(98, 33);
+            this.gameButton3.Size = new System.Drawing.Size(148, 35);
             this.gameButton3.TabIndex = 2;
             this.gameButton3.Text = "Resume Game";
             this.gameButton3.UseVisualStyleBackColor = false;
@@ -150,11 +204,11 @@
             | System.Windows.Forms.AnchorStyles.Right)));
             this.gameButton2.BackColor = System.Drawing.Color.Black;
             this.gameButton2.ForeColor = System.Drawing.Color.CornflowerBlue;
-            this.gameButton2.Location = new System.Drawing.Point(5, 47);
+            this.gameButton2.Location = new System.Drawing.Point(5, 49);
             this.gameButton2.Margin = new System.Windows.Forms.Padding(5, 5, 5, 4);
             this.gameButton2.Name = "gameButton2";
             this.gameButton2.Padding = new System.Windows.Forms.Padding(4);
-            this.gameButton2.Size = new System.Drawing.Size(98, 34);
+            this.gameButton2.Size = new System.Drawing.Size(148, 35);
             this.gameButton2.TabIndex = 1;
             this.gameButton2.Text = "New Game";
             this.gameButton2.UseVisualStyleBackColor = false;
@@ -169,7 +223,7 @@
             this.btnQuickStart.Margin = new System.Windows.Forms.Padding(5, 5, 5, 4);
             this.btnQuickStart.Name = "btnQuickStart";
             this.btnQuickStart.Padding = new System.Windows.Forms.Padding(4);
-            this.btnQuickStart.Size = new System.Drawing.Size(98, 33);
+            this.btnQuickStart.Size = new System.Drawing.Size(148, 35);
             this.btnQuickStart.TabIndex = 0;
             this.btnQuickStart.Text = "Quick Start";
             this.btnQuickStart.UseVisualStyleBackColor = false;
@@ -177,8 +231,9 @@
             // 
             // progressBar1
             // 
-            this.progressBar1.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
-            this.progressBar1.Location = new System.Drawing.Point(189, 447);
+            this.progressBar1.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.tableLayoutPanel1.SetColumnSpan(this.progressBar1, 2);
+            this.progressBar1.Location = new System.Drawing.Point(184, 378);
             this.progressBar1.Margin = new System.Windows.Forms.Padding(0);
             this.progressBar1.Name = "progressBar1";
             this.progressBar1.Size = new System.Drawing.Size(208, 14);
@@ -186,53 +241,13 @@
             this.progressBar1.TabIndex = 4;
             this.progressBar1.Visible = false;
             // 
-            // tblButtonPanel
-            // 
-            this.tblButtonPanel.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
-            this.tblButtonPanel.ColumnCount = 4;
-            this.tblButtonPanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 25F));
-            this.tblButtonPanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 25F));
-            this.tblButtonPanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 25F));
-            this.tblButtonPanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 25F));
-            this.tblButtonPanel.Controls.Add(this.btnQuit, 3, 1);
-            this.tblButtonPanel.Controls.Add(this.btnQuickStart, 0, 0);
-            this.tblButtonPanel.Controls.Add(this.gameButton2, 0, 1);
-            this.tblButtonPanel.Controls.Add(this.gameButton3, 1, 0);
-            this.tblButtonPanel.Controls.Add(this.gameButton4, 1, 1);
-            this.tblButtonPanel.Controls.Add(this.gameButton5, 2, 0);
-            this.tblButtonPanel.Controls.Add(this.gameButton6, 2, 1);
-            this.tblButtonPanel.Controls.Add(this.gameButton7, 3, 0);
-            this.tblButtonPanel.Location = new System.Drawing.Point(76, 411);
-            this.tblButtonPanel.Name = "tblButtonPanel";
-            this.tblButtonPanel.RowCount = 2;
-            this.tblButtonPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
-            this.tblButtonPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
-            this.tblButtonPanel.Size = new System.Drawing.Size(434, 85);
-            this.tblButtonPanel.TabIndex = 8;
-            // 
-            // lblStatus
-            // 
-            this.lblStatus.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
-            this.lblStatus.AutoSize = true;
-            this.lblStatus.Location = new System.Drawing.Point(93, 384);
-            this.lblStatus.MinimumSize = new System.Drawing.Size(400, 0);
-            this.lblStatus.Name = "lblStatus";
-            this.lblStatus.Size = new System.Drawing.Size(400, 13);
-            this.lblStatus.TabIndex = 9;
-            this.lblStatus.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
-            // 
             // MainMenuForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.Black;
-            this.ClientSize = new System.Drawing.Size(575, 510);
-            this.Controls.Add(this.lblStatus);
-            this.Controls.Add(this.progressBar1);
-            this.Controls.Add(this.tblButtonPanel);
-            this.Controls.Add(this.pictureBox1);
+            this.ClientSize = new System.Drawing.Size(575, 403);
+            this.Controls.Add(this.tableLayoutPanel1);
             this.ForeColor = System.Drawing.Color.White;
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.MaximizeBox = false;
@@ -241,16 +256,18 @@
             this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "FrEee";
+            this.tableLayoutPanel1.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.tblButtonPanel.ResumeLayout(false);
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
         #endregion
 
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
         private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.TableLayoutPanel tblButtonPanel;
         private Controls.GameButton btnQuit;
         private Controls.GameButton gameButton7;
         private Controls.GameButton gameButton6;
@@ -260,8 +277,6 @@
         private Controls.GameButton gameButton2;
         private Controls.GameButton btnQuickStart;
         private System.Windows.Forms.ProgressBar progressBar1;
-        private System.Windows.Forms.TableLayoutPanel tblButtonPanel;
-        private System.Windows.Forms.Label lblStatus;
 
     }
 }
