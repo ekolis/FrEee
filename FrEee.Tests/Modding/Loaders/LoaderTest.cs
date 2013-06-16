be1de453a68663bb385f719ad18ecd0e8f5a3d07
Ed Kolis <edkolis@gmail.com>
2013-06-16 16:31:26 -0400

    Added unit test for loading the stock mod without errors.

---
+++ FrEee.Tests/Modding/Loaders/LoaderTest.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using FrEee.Modding;
+
+namespace FrEee.Tests.Modding.Loaders
+{
+	/// <summary>
+	/// Tests mod loaders.
+	/// </summary>
+	[TestClass]
+	public class LoaderTest
+	{
+		/// <summary>
+		/// Makes sure the stock mod loads with no errors.
+		/// </summary>
+		[TestMethod]
+		public void LoadStockModWithoutErrors()
+		{
+			Mod.Load(null);
+			Assert.AreEqual(0, Mod.Errors.Count);
+		}
+	}
+}
