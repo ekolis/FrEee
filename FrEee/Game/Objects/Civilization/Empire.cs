4f86df4d733bd6f6d7025fcf633103ff1b7a6af4
Ed Kolis <edkolis@gmail.com>
2013-06-22 14:22:25 -0400

    comment fix

--- FrEee/Game/Objects/Civilization/Empire.cs
+++ FrEee/Game/Objects/Civilization/Empire.cs
@@ -309,7 +309,7 @@ namespace FrEee.Game.Objects.Civilization
 			if (item is Tech && ((Tech)item).IsRacial && !this.Abilities.Any(a => a.Name == "Tech Area" && a.Value1 == ((Tech)item).RacialTechID))
 				return false; // racial tech that this empire doesn't have the trait for
 			if (item is Tech && ((Tech)item).IsUnique && !this.UniqueTechsFound.Any(t => t == ((Tech)item).UniqueTechID))
-				return false; // racial tech that this empire doesn't have the trait for
+				return false; // unique tech that this empire hasn't discovered
 			return item.TechnologyRequirements.All(r => ResearchedTechnologies[r.Technology] >= r.Level);
 		}
 
