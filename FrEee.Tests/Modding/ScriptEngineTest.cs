412c26f847e55b5373ffcba3a7d11614e9f8ede0
Ed Kolis <edkolis@gmail.com>
2013-10-08 15:28:44 -0400

    Added unit test for script engine

---
+++ FrEee.Tests/Modding/ScriptEngineTest.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using FrEee.Modding;
+using System.Collections.Generic;
+
+namespace FrEee.Tests.Modding
+{
+	[TestClass]
+	public class ScriptEngineTest
+	{
+		[TestMethod]
+		public void RunScript()
+		{
+			var script = "return x * y;";
+			var variables = new Dictionary<string, object>();
+			variables.Add("x", 6);
+			variables.Add("y", 7);
+			Assert.AreEqual(42, (int)ScriptEngine.RunScript(script, variables));
+		}
+
+		[TestMethod]
+		public void CallFunction()
+		{
+			var script =
+				@"
+					def square(x):
+						return x ^ 2;
+				";
+			Assert.AreEqual(64, (int)ScriptEngine.CallFunction(script, "square", 8));
+		}
+	}
+}
