a93d0b258fcb61a52454291218821faddab090c8
Ed Kolis <edkolis@gmail.com>
2013-10-13 19:01:46 -0400

    Updated script

--- FrEee/Pictures/Races/AI_Default/AI.py
+++ FrEee/Pictures/Races/AI_Default/AI.py
@@ -21,7 +21,7 @@ if (enabledMinisters.ContainsKey("Empire Management")):
 		# choose what to research
 		# TODO - actually choose sensibly, don't always research Propulsion
 		cmd = ResearchCommand(empire);
-		techs = galaxy.Mod.Technologies;
+		techs = galaxy.Settings.Mod.Technologies;
 		tech = techs.Single(lambda t: t.Name == "Propulsion");
 		# why isn't this line not working?!
 		#cmd.SetSpending(galaxy.Mod.Technologies.Find("Propulsion"), 100);
