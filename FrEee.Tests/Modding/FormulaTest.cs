acbb35c1290f9493c61691876f90186acec7a2ef
Ed Kolis <edkolis@gmail.com>
2013-10-16 15:42:35 -0400

    Added framework for formula fields in mod files. Still need to make the data files actually run the formulas, though.

---
+++ FrEee.Tests/Modding/FormulaTest.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using FrEee.Modding;
+
+namespace FrEee.Tests.Modding
+{
+	/// <summary>
+	/// Tests data file formulas.
+	/// </summary>
+	[TestClass]
+	public class FormulaTest
+	{
+		/// <summary>
+		/// Tests static formulas.
+		/// </summary>
+		[TestMethod]
+		public void StaticFormula()
+		{
+			var data =
+@"Parameter Name := speed
+Parameter Minimum := 3
+Parameter Maximum := 5
+Parameter Name := warhead
+Parameter Maximum := 5
+Name := ='Nuclear Missile ' + warhead.ToRomanNumeral() + ' S' + str(speed)";
+
+			var metarec = new MetaRecord(data.Split('\n'));
+
+			Assert.AreEqual(2, metarec.Parameters.Count());
+			Assert.AreEqual(3, metarec.Parameters.First().Minimum);
+			Assert.AreEqual(5, metarec.Parameters.First().Maximum);
+			Assert.AreEqual(1, metarec.Parameters.Last().Minimum);
+			Assert.AreEqual(5, metarec.Parameters.Last().Maximum);
+
+			var recs = metarec.Instantiate();
+			Assert.AreEqual(15, recs.Count());
+			int index = 0;
+			Assert.AreEqual(1, recs.Where(r => r.GetString("Name", ref index) == "Nuclear Missile III S4").Count());
+		}
+	}
+}
