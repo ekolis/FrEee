0613e380705225a6c7c5f0ef155ff0bb6b56847c
Ed Kolis <edkolis@gmail.com>
2013-10-20 07:05:50 -0400

    Starting on empire list form

---
+++ FrEee.WinForms/Forms/EmpireListForm.Designer.cs
@@ -0,0 +1,100 @@
+﻿namespace FrEee.WinForms.Forms
+{
+	partial class EmpireListForm
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.gamePanel1 = new FrEee.WinForms.Controls.GamePanel();
+			this.lstEmpires = new System.Windows.Forms.ListView();
+			this.btnClose = new FrEee.WinForms.Controls.GameButton();
+			this.gamePanel1.SuspendLayout();
+			this.SuspendLayout();
+			// 
+			// gamePanel1
+			// 
+			this.gamePanel1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+			this.gamePanel1.BackColor = System.Drawing.Color.Black;
+			this.gamePanel1.BorderColor = System.Drawing.Color.CornflowerBlue;
+			this.gamePanel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+			this.gamePanel1.Controls.Add(this.lstEmpires);
+			this.gamePanel1.ForeColor = System.Drawing.Color.White;
+			this.gamePanel1.Location = new System.Drawing.Point(12, 12);
+			this.gamePanel1.Name = "gamePanel1";
+			this.gamePanel1.Padding = new System.Windows.Forms.Padding(3);
+			this.gamePanel1.Size = new System.Drawing.Size(696, 169);
+			this.gamePanel1.TabIndex = 1;
+			// 
+			// lstEmpires
+			// 
+			this.lstEmpires.BackColor = System.Drawing.Color.Black;
+			this.lstEmpires.BorderStyle = System.Windows.Forms.BorderStyle.None;
+			this.lstEmpires.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.lstEmpires.ForeColor = System.Drawing.Color.White;
+			this.lstEmpires.Location = new System.Drawing.Point(3, 3);
+			this.lstEmpires.Name = "lstEmpires";
+			this.lstEmpires.Size = new System.Drawing.Size(688, 161);
+			this.lstEmpires.TabIndex = 1;
+			this.lstEmpires.UseCompatibleStateImageBehavior = false;
+			this.lstEmpires.ItemSelectionChanged += new System.Windows.Forms.ListViewItemSelectionChangedEventHandler(this.lstEmpires_ItemSelectionChanged);
+			// 
+			// btnClose
+			// 
+			this.btnClose.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+			this.btnClose.BackColor = System.Drawing.Color.Black;
+			this.btnClose.ForeColor = System.Drawing.Color.CornflowerBlue;
+			this.btnClose.Location = new System.Drawing.Point(633, 566);
+			this.btnClose.Name = "btnClose";
+			this.btnClose.Size = new System.Drawing.Size(75, 23);
+			this.btnClose.TabIndex = 2;
+			this.btnClose.Text = "Close";
+			this.btnClose.UseVisualStyleBackColor = false;
+			this.btnClose.Click += new System.EventHandler(this.btnClose_Click);
+			// 
+			// EmpireListForm
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.BackColor = System.Drawing.Color.Black;
+			this.ClientSize = new System.Drawing.Size(720, 601);
+			this.Controls.Add(this.btnClose);
+			this.Controls.Add(this.gamePanel1);
+			this.ForeColor = System.Drawing.Color.White;
+			this.Name = "EmpireListForm";
+			this.Text = "Empires";
+			this.gamePanel1.ResumeLayout(false);
+			this.ResumeLayout(false);
+
+		}
+
+		#endregion
+
+		private Controls.GamePanel gamePanel1;
+		private System.Windows.Forms.ListView lstEmpires;
+		private Controls.GameButton btnClose;
+	}
+}
