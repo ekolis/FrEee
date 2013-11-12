1639cbc0df41043149bb9d1e8066ad9be556a716
Ed Kolis <edkolis@gmail.com>
2013-11-12 14:41:31 -0500

    More null ability rule bugs

--- FrEee/Game/Objects/Civilization/Empire.cs
+++ FrEee/Game/Objects/Civilization/Empire.cs
@@ -386,7 +386,7 @@ namespace FrEee.Game.Objects.Civilization
 			if (item == null)
 				return true;
 			// TODO - racial/unique tech should just be requirements
-			if (item is Tech && ((Tech)item).IsRacial && !this.Abilities().Any(a => a.Rule.Matches("Tech Area") && a.Value1 == ((Tech)item).RacialTechID))
+			if (item is Tech && ((Tech)item).IsRacial && !this.Abilities().Any(a => a.Rule != null && a.Rule.Matches("Tech Area") && a.Value1 == ((Tech)item).RacialTechID))
 				return false; // racial tech that this empire doesn't have the trait for
 			if (item is Tech && ((Tech)item).IsUnique && !this.UniqueTechsFound.Any(t => t == ((Tech)item).UniqueTechID))
 				return false; // unique tech that this empire hasn't discovered
