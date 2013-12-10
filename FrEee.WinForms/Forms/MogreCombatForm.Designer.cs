6ddbe471abeda67785ce56cea0fdbb50879aa578
se5a <se5a@localhost>
2013-12-10 15:22:31 +1300

    why'd this disapear?

---
+++ FrEee.WinForms/Forms/MogreCombatForm.Designer.cs
@@ -0,0 +1,189 @@
+﻿namespace FrEee.WinForms.Forms
+{
+    partial class MogreCombatForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.gameTableLayoutPanel1 = new FrEee.WinForms.Controls.GameTableLayoutPanel();
+            this.gameTableLayoutPanel2 = new FrEee.WinForms.Controls.GameTableLayoutPanel();
+            this.gameButton1 = new FrEee.WinForms.Controls.GameButton();
+            this.gameButton2 = new FrEee.WinForms.Controls.GameButton();
+            this.gameButton3 = new FrEee.WinForms.Controls.GameButton();
+            this.gameButton4 = new FrEee.WinForms.Controls.GameButton();
+            this.gameButton5 = new FrEee.WinForms.Controls.GameButton();
+            this.gameButton6 = new FrEee.WinForms.Controls.GameButton();
+            this.gameTableLayoutPanel1.SuspendLayout();
+            this.gameTableLayoutPanel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.richTextBox1.Location = new System.Drawing.Point(3, 122);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(262, 429);
+            this.richTextBox1.TabIndex = 0;
+            this.richTextBox1.Text = "";
+            // 
+            // gameTableLayoutPanel1
+            // 
+            this.gameTableLayoutPanel1.ColumnCount = 2;
+            this.gameTableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 28.09224F));
+            this.gameTableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 71.90776F));
+            this.gameTableLayoutPanel1.Controls.Add(this.richTextBox1, 0, 1);
+            this.gameTableLayoutPanel1.Controls.Add(this.gameTableLayoutPanel2, 0, 0);
+            this.gameTableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.gameTableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.gameTableLayoutPanel1.Name = "gameTableLayoutPanel1";
+            this.gameTableLayoutPanel1.RowCount = 2;
+            this.gameTableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 21.48014F));
+            this.gameTableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 78.51986F));
+            this.gameTableLayoutPanel1.Size = new System.Drawing.Size(954, 554);
+            this.gameTableLayoutPanel1.TabIndex = 1;
+            // 
+            // gameTableLayoutPanel2
+            // 
+            this.gameTableLayoutPanel2.ColumnCount = 3;
+            this.gameTableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 84F));
+            this.gameTableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 84F));
+            this.gameTableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 84F));
+            this.gameTableLayoutPanel2.Controls.Add(this.gameButton1, 0, 0);
+            this.gameTableLayoutPanel2.Controls.Add(this.gameButton2, 1, 0);
+            this.gameTableLayoutPanel2.Controls.Add(this.gameButton3, 2, 0);
+            this.gameTableLayoutPanel2.Controls.Add(this.gameButton4, 0, 1);
+            this.gameTableLayoutPanel2.Controls.Add(this.gameButton5, 1, 1);
+            this.gameTableLayoutPanel2.Controls.Add(this.gameButton6, 2, 1);
+            this.gameTableLayoutPanel2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.gameTableLayoutPanel2.Location = new System.Drawing.Point(3, 3);
+            this.gameTableLayoutPanel2.Name = "gameTableLayoutPanel2";
+            this.gameTableLayoutPanel2.RowCount = 3;
+            this.gameTableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 40F));
+            this.gameTableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 40F));
+            this.gameTableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 57F));
+            this.gameTableLayoutPanel2.Size = new System.Drawing.Size(262, 113);
+            this.gameTableLayoutPanel2.TabIndex = 0;
+            // 
+            // gameButton1
+            // 
+            this.gameButton1.BackColor = System.Drawing.Color.Black;
+            this.gameButton1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.gameButton1.ForeColor = System.Drawing.Color.CornflowerBlue;
+            this.gameButton1.Location = new System.Drawing.Point(3, 3);
+            this.gameButton1.Name = "gameButton1";
+            this.gameButton1.Size = new System.Drawing.Size(78, 34);
+            this.gameButton1.TabIndex = 0;
+            this.gameButton1.Text = "Back to Start";
+            this.gameButton1.UseVisualStyleBackColor = false;
+            // 
+            // gameButton2
+            // 
+            this.gameButton2.BackColor = System.Drawing.Color.Black;
+            this.gameButton2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.gameButton2.ForeColor = System.Drawing.Color.CornflowerBlue;
+            this.gameButton2.Location = new System.Drawing.Point(87, 3);
+            this.gameButton2.Name = "gameButton2";
+            this.gameButton2.Size = new System.Drawing.Size(78, 34);
+            this.gameButton2.TabIndex = 1;
+            this.gameButton2.Text = "Play";
+            this.gameButton2.UseVisualStyleBackColor = false;
+            // 
+            // gameButton3
+            // 
+            this.gameButton3.BackColor = System.Drawing.Color.Black;
+            this.gameButton3.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.gameButton3.ForeColor = System.Drawing.Color.CornflowerBlue;
+            this.gameButton3.Location = new System.Drawing.Point(171, 3);
+            this.gameButton3.Name = "gameButton3";
+            this.gameButton3.Size = new System.Drawing.Size(88, 34);
+            this.gameButton3.TabIndex = 2;
+            this.gameButton3.Text = "Fast Foward";
+            this.gameButton3.UseVisualStyleBackColor = false;
+            // 
+            // gameButton4
+            // 
+            this.gameButton4.BackColor = System.Drawing.Color.Black;
+            this.gameButton4.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.gameButton4.ForeColor = System.Drawing.Color.CornflowerBlue;
+            this.gameButton4.Location = new System.Drawing.Point(3, 43);
+            this.gameButton4.Name = "gameButton4";
+            this.gameButton4.Size = new System.Drawing.Size(78, 34);
+            this.gameButton4.TabIndex = 3;
+            this.gameButton4.Text = "Previous Ship";
+            this.gameButton4.UseVisualStyleBackColor = false;
+            // 
+            // gameButton5
+            // 
+            this.gameButton5.BackColor = System.Drawing.Color.Black;
+            this.gameButton5.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.gameButton5.ForeColor = System.Drawing.Color.CornflowerBlue;
+            this.gameButton5.Location = new System.Drawing.Point(87, 43);
+            this.gameButton5.Name = "gameButton5";
+            this.gameButton5.Size = new System.Drawing.Size(78, 34);
+            this.gameButton5.TabIndex = 4;
+            this.gameButton5.Text = "Free Cam";
+            this.gameButton5.UseVisualStyleBackColor = false;
+            // 
+            // gameButton6
+            // 
+            this.gameButton6.BackColor = System.Drawing.Color.Black;
+            this.gameButton6.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.gameButton6.ForeColor = System.Drawing.Color.CornflowerBlue;
+            this.gameButton6.Location = new System.Drawing.Point(171, 43);
+            this.gameButton6.Name = "gameButton6";
+            this.gameButton6.Size = new System.Drawing.Size(88, 34);
+            this.gameButton6.TabIndex = 5;
+            this.gameButton6.Text = "Next Ship";
+            this.gameButton6.UseVisualStyleBackColor = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(954, 554);
+            this.Controls.Add(this.gameTableLayoutPanel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.gameTableLayoutPanel1.ResumeLayout(false);
+            this.gameTableLayoutPanel2.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private Controls.GameTableLayoutPanel gameTableLayoutPanel1;
+        private Controls.GameTableLayoutPanel gameTableLayoutPanel2;
+        private Controls.GameButton gameButton1;
+        private Controls.GameButton gameButton2;
+        private Controls.GameButton gameButton3;
+        private Controls.GameButton gameButton4;
+        private Controls.GameButton gameButton5;
+        private Controls.GameButton gameButton6;
+    }
+}
---
+++ FrEee.WinForms/Forms/MogreCombatForm.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace FrEee.WinForms.Forms
+{
+    public partial class MogreCombatForm : Form
+    {
+        public MogreCombatForm()
+        {
+            InitializeComponent();
+        }
+
+        private void tableLayoutPanel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+    }
+}
