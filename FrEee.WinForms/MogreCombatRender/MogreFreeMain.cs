267320a5a11309c9ff50ba387336273053f993af
se5a <se5a@localhost>
2013-12-11 23:35:52 +1300

    ok that's much better. commandAI was not running during replay and hostile ship lists were not being created. think the helm AI is still not quite there though.

--- FrEee.WinForms/MogreCombatRender/MogreFreeMain.cs
+++ FrEee.WinForms/MogreCombatRender/MogreFreeMain.cs
@@ -485,7 +485,7 @@ namespace FrEee.WinForms.MogreCombatRender
 				}
 
 				battletic++;
-
+                cmdfreq_countr++;
 				foreach (CombatObject comObj in battle.CombatObjects)
 				{
 					battle.helm(comObj);
@@ -511,7 +511,8 @@ namespace FrEee.WinForms.MogreCombatRender
 
 				if (cmdfreq_countr >= Battle_Space.CommandFrequency)
 					cmdfreq_countr = 0;
-
+                
+                //battletic++;
 				Application.DoEvents();
 			}
 		}
@@ -568,10 +569,7 @@ namespace FrEee.WinForms.MogreCombatRender
             toTargetLine.Position(new Vector3((float)comObj.waypointTarget.comObj.cmbt_loc.X, (float)comObj.waypointTarget.comObj.cmbt_loc.Y, (float)comObj.waypointTarget.comObj.cmbt_loc.Z));
             toTargetLine.End();
 
-            if (comObj.icomobj.ID == 3299939791363671165)
-            {
-                bool p = true;
-            }
+
             //mSceneMgr.DestroyManualObject("toOtherNodeline" + IDName);
             //ManualObject toOtherNodeline = mSceneMgr.CreateManualObject("toOtherNodeline" + IDName);
             //node.AttachObject(toOtherNodeline);
