0b71b39353fe1bf7d1c5c20075e6e20809a400de
Ed Kolis <edkolis@gmail.com>
2013-12-12 17:15:15 -0500

    Trying to fix the battlespace unit test. But it appears that we need actual space objects in a sector in order to initialize the empires-in-combat list...

--- FrEee.Tests/Game/Objects/Combat2/Battle_SpaceTests.cs
+++ FrEee.Tests/Game/Objects/Combat2/Battle_SpaceTests.cs
@@ -3,6 +3,8 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 using FrEee.Game.Objects.Combat2;
 using FrEee.Game.Objects.Vehicles;
 using FrEee.Game.Objects.Civilization;
+using FrEee.Game.Objects.Space;
+using System.Drawing;
 
 namespace FrEee.Tests.Game.Objects.Combat2
 {
@@ -13,13 +15,15 @@ namespace FrEee.Tests.Game.Objects.Combat2
         [TestMethod]
         public void turnship()
         {
+			var gal = new Galaxy(); // always need one of these...
+			var sys = new StarSystem(1);
 			var emp = new Empire();
 			var combatant = new MockCombatant(emp);
             var testComObj = new CombatObject(combatant, 42);
 			testComObj.maxRotate = 5;
 			testComObj.cmbt_accel = new Point3d(0, 0);
 			testComObj.maxStrafeThrust = 0;
-			var battle = new Battle_Space(null);
+			var battle = new Battle_Space(new Sector(sys, new Point()));
 			battle.Combatants.Add(combatant);
 			battle.CombatObjects.Add(testComObj);
 
