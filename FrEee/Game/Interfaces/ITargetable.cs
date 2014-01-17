d07aaade969f58fbdf5ea77ff0bfce47c7a2e762
Ed Kolis <edkolis@gmail.com>
2014-01-16 19:25:04 -0500

    Added ITargetable interface, which is a slimmed down version of ICombatant that we can use for seekers. Seeker still implements ICombatant, though, for now.

---
+++ FrEee/Game/Interfaces/ITargetable.cs
@@ -0,0 +1,19 @@
+﻿using FrEee.Game.Enumerations;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace FrEee.Game.Interfaces
+{
+	/// <summary>
+	/// Something which can be specifically target by weapons.
+	/// </summary>
+	public interface ITargetable : IDamageable, IOwnable
+	{
+		/// <summary>
+		/// What type of object is this for weapon targeting purposes?
+		/// </summary>
+		WeaponTargets WeaponTargetType { get; }
+	}
+}
