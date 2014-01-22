841ebd8ff9e7e8702f0840753e8778880c196b2e
se5a <se5a@gmail.com>
2014-01-23 08:52:36 +1300

    Fixed PythagC test.

--- FrEee.Tests/Game/Objects/Combat2/Point3dTests.cs
+++ FrEee.Tests/Game/Objects/Combat2/Point3dTests.cs
@@ -12,11 +12,14 @@ namespace FrEee.Tests.Game.Objects.Combat2
         {
             Fix16 side_at0 = (Fix16)3;
             Fix16 side_bt0 = (Fix16)4;
-            Assert.AreEqual(5, Trig.pythagC(side_at0, side_bt0));
+            Fix16 expectedResult = 5;
+            Assert.AreEqual(expectedResult, Trig.pythagC(side_at0, side_bt0));
 
             Fix16 side_at1 = (Fix16)5;
             Fix16 side_bt1 = (Fix16)12;
-            Assert.AreEqual(13,Trig.pythagC(side_at1, side_bt1));
+
+            expectedResult = 13;
+            Assert.AreEqual(expectedResult, Trig.pythagC(side_at1, side_bt1));
         }
 
         [TestMethod]
@@ -28,15 +31,20 @@ namespace FrEee.Tests.Game.Objects.Combat2
             Point3d p2 = new Point3d(10, 0, 0);
             Point3d v2 = new Point3d(0, 0, 0);
 
-            Fix16 result = 5;
+            Fix16 expectedResult = 5;
 
-            Assert.AreEqual(result, GravMath.closingRate(p1, v1, p2, v2));       
+            Assert.AreEqual(expectedResult, GravMath.closingRate(p1, v1, p2, v2));       
         }
 
-        //[TestMethod]
-        //public void distance()
-        //{
-        //    Point3d p1 = new Point3d(
-        //}
+        [TestMethod]
+        public void distance()
+        {
+            Point3d p1 = new Point3d(0,0,0);
+            Point3d p2 = new Point3d(0, 10, 0);
+            Fix16 expectedResult = 10;
+
+            Assert.AreEqual(expectedResult, Trig.distance(p1, p2));
+
+        }
     }
 }
