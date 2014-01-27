1af1d2d6af071a441a596da99689cbd691c2785b
Ed Kolis <edkolis@gmail.com>
2014-01-27 17:21:52 -0500

    Updated assembly info, next release we'll at least have the right version number!

--- FrEee.WinForms/Properties/AssemblyInfo.cs
+++ FrEee.WinForms/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("FrEee.WinForms")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Open source Space Empires IV clone")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("FrEee.WinForms")]
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.0.4.0")]
-[assembly: AssemblyFileVersion("0.0.4.0")]
+[assembly: AssemblyVersion("0.0.7.0")]
+[assembly: AssemblyFileVersion("0.0.7.0")]
