8a981778a6ceffb2626e9d390d651a87d711559e
Ed Kolis <edkolis@gmail.com>
2014-01-30 19:50:27 -0500

    Sort of fixed serialization? But now all sorts of objects like technologies and populations are missing. It appears that the serializer is constructing a blank object, assigning it to my property, and manipulating the property value. Which is useless when the property is just a proxy, because "manipulating" its value just causes any changes to be thrown away. Maybe I need some of those TypeConverter things?

---
+++ FrEee/Game/Objects/Civilization/RacePopulation.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace FrEee.Game.Objects.Civilization
+{
+	/// <summary>
+	/// A race with a quantity of population.
+	/// </summary>
+	public class RacePopulation
+	{
+		/// <summary>
+		/// For serialization.
+		/// </summary>
+		public RacePopulation()
+		{
+
+		}
+
+		public RacePopulation(Race race, long pop)
+		{
+			Race = race;
+			Population = pop;
+		}
+
+		public Race Race { get; set; }
+
+		public long Population { get; set; }
+	}
+}
