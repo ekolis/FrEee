75f3a884efcde87e2dc9f9856296630d0b8287f6
se5a <se5a@gmail.com>
2014-02-07 08:35:23 +1300

    add ship and effects classes

---
+++ FrEee.WinForms/MogreCombatRender/ShipCfg.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+using SpaceMath;
+using Newtonsoft.Json;
+
+namespace Mogreoids
+{
+    class ShipCfg
+    {
+        //public string MainMesh { get; set; }
+        public MainMesh_Properties MainMesh { get; set; }
+        public Dictionary<string, Dictionary<string, Effect>> Effects { get; set; }
+        //public Dictionary<string, Effect> ForwardThrust_Effect
+        //{
+        //    get { return Effects["ForwardThrust_Effect"]; }
+        //}
+        //public Dictionary<string, string> getdata { get { return data; } }
+    }
+
+    class MainMesh_Properties
+    {
+        public string Name { get;set;}
+        public float[] Scale {get; set;}
+    }
+
+    class Effect
+    {
+        public string ParticleEffect { get; set; }
+        public float[] Loc { get; set; }
+        public float[] Dir { get; set; }
+    }
+}
