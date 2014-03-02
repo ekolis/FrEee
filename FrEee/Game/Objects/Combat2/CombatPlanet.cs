ea25585f81cb8ad622c8441f6a5a51df5b405ae5
Ed Kolis <edkolis@gmail.com>
2014-03-02 15:15:31 -0500

    Planets can sort of fight in combat. But they're invisible, and ships won't shoot at them.

---
+++ FrEee/Game/Objects/Combat2/CombatPlanet.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using FrEee.Utility;
+using FrEee.Utility.Extensions;
+
+using FrEee.Game.Interfaces;
+using FrEee.Game.Objects.Vehicles;
+using FrEee.Game.Objects.Combat;
+using FrEee.Game.Objects.Technology;
+using FrEee.Modding;
+
+using NewtMath.f16;
+using FixMath.NET;
+using FrEee.Game.Objects.Space;
+
+namespace FrEee.Game.Objects.Combat2
+{
+	public class CombatPlanet : ControlledCombatObject
+	{
+		public CombatPlanet(Planet start, Planet working, int battleseed, string IDPrefix = "PLN")
+			: base(start, working, new PointXd(0, 0, 0), new PointXd(0, 0, 0), battleseed, IDPrefix)
+		{
+			// TODO - planets that can move in combat?
+			cmbt_mass = Fix16.MaxValue;
+			maxfowardThrust = 0;
+			maxStrafeThrust = 0;
+			maxRotate = 0;
+		}
+
+		#region fields & properties
+
+		/// <summary>
+		/// The planet's state at the start of combat.
+		/// </summary>
+		public Planet StartPlanet { get { return (Planet)StartCombatant; } private set { StartCombatant = value; } }
+
+		/// <summary>
+		/// The current state of the planet.
+		/// </summary>
+		public Planet WorkingPlanet { get { return (Planet)WorkingObject; } private set { WorkingObject = value; } }
+
+		#endregion
+
+		#region methods and functions
+		public override void renewtoStart()
+		{
+
+#if DEBUG
+            Console.WriteLine("renewtoStart for CombatPlanet");
+#endif
+			var planet = StartPlanet.Copy();
+			planet.IsMemory = true;
+			if (planet.Owner != StartPlanet.Owner)
+				planet.Owner.Dispose(); // don't need extra empires!
+			if (planet.Colony != null)
+			{
+				planet.Colony.Owner = StartPlanet.Owner;
+
+				// copy over the facilities individually so they can take damage without affecting the starting state
+				planet.Colony.Facilities.Clear();
+#if DEBUG
+				Console.WriteLine("copying facilities");
+#endif
+				foreach (var f in (StartPlanet.Colony.Facilities))
+				{
+					var fcopy = f.Copy();
+					planet.Colony.Facilities.Add(fcopy);
+#if DEBUG
+					Console.Write(".");
+#endif
+				}
+#if DEBUG
+				Console.WriteLine("Done");
+#endif
+			}
+
+			WorkingPlanet = planet;
+			RefreshWeapons();
+
+			foreach (var w in Weapons)
+				w.nextReload = 1;
+
+            base.renewtoStart();
+#if DEBUG
+            Console.WriteLine("Done");
+#endif
+		}
+
+		protected override void RefreshWeapons()
+		{
+			var weapons = new List<CombatWeapon>();
+			weapons = new List<CombatWeapon>();
+#if DEBUG
+            Console.WriteLine("RefreshingWeapons");
+#endif
+			foreach (Component weapon in WorkingPlanet.Weapons)
+			{
+				CombatWeapon wpn = new CombatWeapon(weapon);
+				weapons.Add(wpn);
+#if DEBUG
+                Console.Write(".");
+#endif
+			}
+			Weapons = weapons;
+#if DEBUG
+            Console.WriteLine("Done");
+#endif
+		}
+
+		#endregion
+	}
+}
--- FrEee/Game/Objects/Combat2/CombatVehicle.cs
+++ FrEee/Game/Objects/Combat2/CombatVehicle.cs
@@ -16,18 +16,16 @@ using FixMath.NET;
 
 namespace FrEee.Game.Objects.Combat2
 {
-	public class CombatVehicle : CombatObject
+	public class CombatVehicle : ControlledCombatObject
 	{
 		public CombatVehicle(Vehicle start_v, Vehicle working_v, int battleseed, string IDPrefix = "SHP")
-			: base(working_v, new PointXd(0, 0, 0), new PointXd(0, 0, 0), start_v.ID, IDPrefix)
+			: base(start_v, working_v, new PointXd(0, 0, 0), new PointXd(0, 0, 0), battleseed, IDPrefix)
 		{
+			// TODO - don't some mods have vehicles >32MT?
 			cmbt_mass = (Fix16)working_v.Size;
 			maxfowardThrust = (Fix16)working_v.Speed * this.cmbt_mass * (Fix16)0.1;
 			maxStrafeThrust = ((Fix16)working_v.Speed * this.cmbt_mass * (Fix16)0.1) / ((Fix16)4 - (Fix16)working_v.Evasion * (Fix16)0.01);
 			maxRotate = ((Fix16)working_v.Speed * this.cmbt_mass * (Fix16)0.1) / ((Fix16)12000 - (Fix16)working_v.Evasion * (Fix16)0.1);
-			StartVehicle = start_v;
-			newDice(battleseed);
-			RefreshWeapons();
 		}
 
 		#region fields & properties
@@ -35,9 +33,7 @@ namespace FrEee.Game.Objects.Combat2
 		/// <summary>
 		/// The vehicle's state at the start of combat.
 		/// </summary>
-		public Vehicle StartVehicle { get; private set; }
-
-		public List<CombatWeapon> weaponList { get; set; }
+		public Vehicle StartVehicle { get { return (Vehicle)StartCombatant; } private set { StartCombatant = value; } }
 
 		/// <summary>
 		/// The current state of the vehicle.
@@ -60,7 +56,6 @@ namespace FrEee.Game.Objects.Combat2
 			ship.Owner = StartVehicle.Owner;
 
 			// copy over the components individually so they can take damage without affecting the starting state
-			// TODO - deal with planets in combat
 			ship.Components.Clear();
 #if DEBUG
             Console.WriteLine("copying components");
@@ -81,7 +76,7 @@ namespace FrEee.Game.Objects.Combat2
 			WorkingVehicle = ship;
 			RefreshWeapons();
 
-			foreach (var w in weaponList)
+			foreach (var w in Weapons)
 				w.nextReload = 1;
 
             base.renewtoStart();
@@ -90,121 +85,24 @@ namespace FrEee.Game.Objects.Combat2
 #endif
 		}
 
-		private void RefreshWeapons()
+		protected override void RefreshWeapons()
 		{
-			this.weaponList = new List<CombatWeapon>();
+			var weapons = new List<CombatWeapon>();
 #if DEBUG
             Console.WriteLine("RefreshingWeapons");
 #endif
 			foreach (Component weapon in WorkingVehicle.Weapons)
 			{
 				CombatWeapon wpn = new CombatWeapon(weapon);
-				this.weaponList.Add(wpn);
+				weapons.Add(wpn);
 #if DEBUG
                 Console.Write(".");
 #endif
 			}
+			Weapons = weapons;
 #if DEBUG
             Console.WriteLine("Done");
 #endif
-		}
-
-		/// <summary>
-		/// endgoal for helm is for the  ship to get to and match speed with the comObj.targetWaypiont as fast as possible.
-		/// the strategic AI script should be responsible for setting where the waypoint is and where thet waypoint is going. 
-		/// </summary>
-		/// <param name="comObj"></param>
-		public override void helm()
-		{
-
-
-			this.debuginfo += "HelmInfo:" + "\r\n";
-			var ship = this.WorkingVehicle;
-			string name = ship.Name;
-			//rotate ship
-			Fix16 timetoturn = (Fix16)0;
-			//Compass angletoturn = new Compass(Trig.angleto(comObj.cmbt_face, comObj.waypointTarget.cmbt_loc));
-			combatWaypoint wpt = this.waypointTarget;
-			Compass angletoWaypoint = new Compass(this.cmbt_loc, this.waypointTarget.cmbt_loc); //relitive to me. 
-			Compass angletoturn = new Compass(angletoWaypoint.Radians - this.cmbt_head.Radians);
-			PointXd vectortowaypoint = this.cmbt_loc - this.waypointTarget.cmbt_loc;
-			//if (comObj.lastVectortoWaypoint != null)
-			//    angletoturn.Radians = Trig.angleA(vectortowaypoint - comObj.lastVectortoWaypoint);
-
-			timetoturn = angletoturn.Radians / this.maxRotate;
-			Fix16 oneEightytime = Fix16.Pi / this.maxRotate;
-			//PointXd offsetVector = comObj.waypointTarget.cmbt_vel - comObj.cmbt_vel; // O = a - b
-			//PointXd combinedVelocity = comObj.cmbt_vel - comObj.waypointTarget.cmbt_vel;
-			//PointXd distancePnt = comObj.waypointTarget.cmbt_loc - comObj.cmbt_loc;
-			//double closingSpeed = Trig.dotProduct(combinedVelocity, distancePnt);
-			Fix16 closingSpeed = NMath.closingRate(this.cmbt_loc, this.cmbt_vel, this.waypointTarget.cmbt_loc, this.waypointTarget.cmbt_vel);
-
-			Fix16 myspeed = Trig.hypotinuse(this.cmbt_vel);
-
-			Fix16 timetokill_ClosingSpeed = closingSpeed / (this.maxfowardThrust / this.cmbt_mass); //t = v / a
-			Fix16 strafetimetokill_ClosingSpeed = closingSpeed / (this.maxStrafeThrust / this.cmbt_mass);
-			Fix16 timetokill_MySpeed = myspeed / (this.maxfowardThrust / this.cmbt_mass);
-
-
-			Fix16 distance = Trig.distance(this.waypointTarget.cmbt_loc, this.cmbt_loc);
-
-
-			Fix16 nominaldistance = this.maxStrafeThrust;
-			Fix16 timetowpt = distance / closingSpeed;
-
-			bool? thrustToWaypoint = true;
-			string helmdo = "";
-
-			if (closingSpeed > (Fix16)0) //getting closer
-			{
-				if (distance <= nominaldistance)  //close to the waypoint.
-				{
-					thrustToWaypoint = null;//should attempt to match speed
-				}
-				if (timetowpt <= timetokill_ClosingSpeed + oneEightytime)//if/when we're going to overshoot teh waypoint.
-				{
-					if (timetowpt < strafetimetokill_ClosingSpeed) //if time to waypoint is less than time to kill speed with strafe thrusters
-					{
-
-						thrustToWaypoint = false;
-					}
-					else
-					{ //use strafe thrust to get close to the waypoint. 
-
-						helmdo = "null" + "\r\n";
-						thrustToWaypoint = null; //else match speed and use thrusters to get closer
-					}
-				}
-			}
-			else
-			{
-			}
-
-			if (thrustToWaypoint == false)
-			{
-				helmdo = "Initiating Turnaround" + "\r\n"; //turn around and thrust the other way
-				angletoturn.Degrees = (angletoWaypoint.Degrees - (Fix16)180) - this.cmbt_head.Degrees; //turn around and thrust the other way
-				angletoturn.normalize();
-			}
-			else if (thrustToWaypoint == null)
-			{
-				angletoturn.Radians = Trig.angleA(this.waypointTarget.cmbt_vel);
-			}
-
-			this.debuginfo += "timetowpt:\t" + timetowpt.ToString() + "\r\n";
-			this.debuginfo += "strafetime:\t" + strafetimetokill_ClosingSpeed.ToString() + "\r\n";
-			this.debuginfo += "speedkilltime:\t" + timetokill_ClosingSpeed.ToString() + "\r\n";
-			this.debuginfo += "180time:\t" + oneEightytime.ToString() + "\r\n";
-			this.debuginfo += "ThrustTo:\t" + thrustToWaypoint.ToString() + "\r\n";
-			this.debuginfo += helmdo + "\r\n";
-
-			turnship(angletoturn, angletoWaypoint);
-
-			thrustship(angletoturn, thrustToWaypoint);
-
-			this.lastVectortoWaypoint = vectortowaypoint;
-
-
 		}
 		#endregion
 	}
