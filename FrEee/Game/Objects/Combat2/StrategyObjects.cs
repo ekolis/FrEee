cf8509e4c366dcc4994e72a193b68771602b9c92
se5a <se5a@localhost>
2014-04-12 09:57:38 +1200

    some work on strategies. I think I have an idea of where I'm going now got an strategy object class now. just have to link it into the ships and into the gui.

--- FrEee/Game/Objects/Combat2/StrategyObjects.cs
+++ FrEee/Game/Objects/Combat2/StrategyObjects.cs
@@ -9,40 +9,45 @@ namespace FrEee.Game.Objects.Combat2
 {
     class StrategyObjects
     {
-        StrategyBaseObj[] StratObjs;
+        StrategyBaseObj[] waypointObjs;
+        StrategyBaseObj[] targetObjs;
 
-        public StrategyObjects(StrategyBaseObj[] stratObjs)
+        public StrategyObjects(StrategyBaseObj[] waypointstratObjs, StrategyBaseObj[] targetstratObjs)
         {
-            this.StratObjs = stratObjs;
+            this.waypointObjs = waypointstratObjs;
+            this.targetObjs = targetstratObjs;
         }
 
         public combatWaypoint calcWaypiont()
-        { 
-            combatWaypoint wp = new combatWaypoint();
-            Object obj = null;
-            foreach (StrategyBaseObj stratobj in StratObjs)
-            {
-                obj = stratobj.get;
-            }
-            if (obj.GetType() == typeof(combatWaypoint))
-                wp = (combatWaypoint)obj;
+        {
+            combatWaypoint wp = (combatWaypoint)waypointObjs[0].getOutput;  
             return wp;
+        }
+
+        public CombatObject calcTarget()
+        {
+            CombatObject tgt = (CombatObject)targetObjs[0].getOutput;           
+            return tgt;
         }
 
     }
 
+ 
 
     class StrategyBaseObj
     {
         protected Object[] inputs;
         protected Type[] inputtypes;
+        protected StrategyBaseObj[] inputLnks { get; set; }
+        protected bool hasvalidInputs = false;
 
         protected Object output = null;
         protected Type outputType;
+        protected StrategyBaseObj[] outputLnks { get; set; }
 
-        public StrategyBaseObj(Object[] inputs, Type[] inputtypes, Type outputtype)
+        public StrategyBaseObj(Type[] inputtypes, Type outputtype)
         {
-            this.inputs = inputs;
+            this.inputs = new object[1];
             this.inputtypes = inputtypes;
             this.outputType = outputtype;
         }
@@ -52,7 +57,7 @@ namespace FrEee.Game.Objects.Combat2
             get { return this.inputs; }
         }
 
-        public Object get
+        public Object getOutput
         {
             get
             {
@@ -62,15 +67,45 @@ namespace FrEee.Game.Objects.Combat2
             }
         }
 
-        public void calc()
-        {}
+        public virtual void calc()
+        {
+            foreach (StrategyBaseObj lnk in inputLnks)
+            {
+                if (lnk.output == null)
+                {
+                    lnk.calc();
+                }
+            }
+        }
         
+    }
+
+    class StrategyWayPoint : StrategyBaseObj
+    {
+        public StrategyWayPoint():base(new Type[2]{typeof(PointXd), typeof(PointXd)}, typeof(combatWaypoint))
+        {
+        }
+        public override void calc()
+        {
+            base.calc();
+            output = new combatWaypoint((PointXd)inputs[0], (PointXd)inputs[1]);
+        }
+    }
+
+    class StrategyComObj : StrategyBaseObj
+    {
+        public StrategyComObj()
+            : base(new Type[1] { typeof(CombatObject) }, typeof(CombatObject))
+        {
+        }
+
     }
 
     class StrategyClosest:StrategyBaseObj
     {
         Type filter = typeof(CombatObject);
-        public StrategyClosest(CombatObject fromObj, List<CombatObject> comObjList, Type filter = null):base (new Object[2]{fromObj, comObjList}, new Type[2]{typeof(CombatObject), typeof(List<CombatObject>)}, typeof(CombatObject))
+        public StrategyClosest(CombatObject fromObj, List<CombatObject> comObjList, Type filter = null):
+            base (new Type[2]{typeof(CombatObject), typeof(List<CombatObject>)}, typeof(CombatObject))
         {
             if (filter != null)
             {
@@ -80,6 +115,7 @@ namespace FrEee.Game.Objects.Combat2
 
         public override void calc()
         {
+            base.calc();
             List<CombatObject> comObjects = (List<CombatObject>)inputs[1];
             CombatObject thisObj = (CombatObject)inputs[0];
             Fix16 distance = Fix16.MaxValue;
@@ -97,6 +133,7 @@ namespace FrEee.Game.Objects.Combat2
                         closest = comObj;
                     }
                 }
+                output = closest;
             }
         }
     }
