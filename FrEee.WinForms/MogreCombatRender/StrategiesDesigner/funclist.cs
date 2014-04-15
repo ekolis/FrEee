27ff53f170746ad5911010ffef594ca92f954c8b
se5a <se5a@localhost>
2014-04-16 00:39:46 +1200

    clicking the 'Save' button on the strategy designer form will now create a strategy object. it still doesn't know what to do to save it though.

--- FrEee.WinForms/MogreCombatRender/StrategiesDesigner/funclist.cs
+++ FrEee.WinForms/MogreCombatRender/StrategiesDesigner/funclist.cs
@@ -20,37 +20,25 @@ namespace FrEee.WinForms.MogreCombatRender.StrategiesDesigner
         public funclist(StratMainForm parent, Canvasdata canvasdata)
         {
             InitializeComponent();
-            /*
-            UCFunct_Loc functloc = new UCFunct_Loc(parent, canvasdata);
-            UCFunct_Vel functvel = new UCFunct_Vel(parent, canvasdata);
-            UCData_This datathis = new UCData_This(parent, canvasdata);
-            UCData_Clock dataclk = new UCData_Clock(parent, canvasdata);
-            UCData_NearestEnemy nearenme = new UCData_NearestEnemy(parent, canvasdata);
-            UCData_EnemyShips nmeshps = new UCData_EnemyShips(parent, canvasdata);
-            UCData_FrendlyShips frndshps = new UCData_FrendlyShips(parent, canvasdata);
-            UCData_NearestComObj nearObj = new UCData_NearestComObj(parent, canvasdata);
-            functItems.Add(nearObj.name, nearObj);
-            functItems.Add(functloc.name, functloc);
-            functItems.Add(functvel.name, functvel);
-            functItems.Add(datathis.name, datathis);
-            functItems.Add(dataclk.name, dataclk);
-            functItems.Add(nearenme.name, nearenme);
-            functItems.Add(nmeshps.name, nmeshps);
-            functItems.Add(frndshps.name, frndshps);
-            */
 
             UCStratBlock ThisObj = new UCStratBlock(new StrategyThisObj(), parent, canvasdata);
             UCStratBlock Closesed = new UCStratBlock(new StrategyClosest(null, null), parent, canvasdata);
             UCStratBlock thisobjEnemys = new UCStratBlock(new StrategyThisEnemys(), parent, canvasdata);
             UCStratBlock loc = new UCStratBlock(new StrategyLocdata(), parent, canvasdata);
             UCStratBlock vel = new UCStratBlock(new StrategyVeldata(), parent, canvasdata);
+            UCStratBlock thisobjempireobj = new UCStratBlock(new StrategyThisEmpireObj(), parent, canvasdata);
+            UCStratBlock mass = new UCStratBlock(new StrategyMassdata(), parent, canvasdata);
+            UCStratBlock range = new UCStratBlock(new StrategyRange(), parent, canvasdata);
 
+            
             functItems.Add(ThisObj.name, ThisObj);
             functItems.Add(Closesed.name, Closesed);
             functItems.Add(thisobjEnemys.name, thisobjEnemys);
+            functItems.Add(thisobjempireobj.name, thisobjempireobj);
             functItems.Add(loc.name, loc);
             functItems.Add(vel.name, vel);
-
+            functItems.Add(mass.name, mass);
+            functItems.Add(range.name, range);
 
             this.listBox1.DataSource = functItems.Keys.ToList();
             
