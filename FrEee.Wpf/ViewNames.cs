ab2f3f7851a5489136efa94f07677b135fcc4a16
Kevin Seitz <kseitz10@gmail.com>
2014-04-22 00:32:55 -0500

    Fun with WPF, PRISM, MEF, and all that jazz.

---
+++ FrEee.Wpf/ViewNames.cs
@@ -0,0 +1,8 @@
+﻿namespace FrEee.Wpf
+{
+    public class ViewNames
+    {
+        public const string MainMenuView = "MainMenuView";
+        public const string GameView = "GameView";
+    }
+}
---
+++ FrEee.Wpf/Views/FrEeeViewBase.cs
@@ -0,0 +1,13 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace FrEee.Wpf.Views
+{
+    public abstract class FrEeeViewBase : ContentControl
+    {
+        static FrEeeViewBase()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(FrEeeViewBase), new FrameworkPropertyMetadata(typeof(FrEeeViewBase)));
+        }
+    }
+}
