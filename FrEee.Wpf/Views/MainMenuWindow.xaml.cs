32f0f4249e2c97653cf6e155d2fcf68756793a55
Kevin Seitz <kseitz10@gmail.com>
2014-04-21 20:02:01 -0500

    Moving MainWindow into /Views/, setting target to .NET 4

---
+++ FrEee.Wpf/Views/MainMenuWindow.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows;
+
+namespace FrEee.Wpf.Views
+{
+    /// <summary>
+    /// Interaction logic for MainMenuWindow.xaml
+    /// </summary>
+    public partial class MainMenuWindow : Window
+    {
+        public MainMenuWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
