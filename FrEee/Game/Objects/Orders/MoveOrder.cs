f842176557c9baf1dd2e0c4e126fc5190886a0ec
Ed Kolis <edkolis@gmail.com>
2014-08-02 12:37:59 -0400

    Vehicles are now resupplied by supply depots.

--- FrEee/Game/Objects/Orders/MoveOrder.cs
+++ FrEee/Game/Objects/Orders/MoveOrder.cs
@@ -83,6 +83,20 @@ namespace FrEee.Game.Objects.Orders
 					// consume supplies
 					sobj.BurnMovementSupplies();
 
+					// resupply space vehicles
+					// either this vehicle from other space objects, or other vehicles from this one
+					// TODO - this should really be done AFTER battles...
+					if (gotoSector.HasAbility("Supply Generation", sobj.Owner))
+					{
+						foreach (var v in gotoSector.SpaceObjects.OfType<SpaceVehicle>().Where(v => v.Owner == sobj.Owner))
+							v.SupplyRemaining = v.SupplyStorage;
+					}
+					if (gotoSector.StarSystem.HasAbility("Supply Generation - System", sobj.Owner) || gotoSector.StarSystem.HasAbility("Supply Generation - System"))
+					{
+						foreach (var v in gotoSector.StarSystem.FindSpaceObjects<SpaceVehicle>().Where(v => v.Owner == sobj.Owner))
+							v.SupplyRemaining = v.SupplyStorage;
+					}
+
 					// is it done?
 					if (gotoSector == Destination)
 						IsComplete = true;
