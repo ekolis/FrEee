c07791dd446c6aad35365aca61d450c9f5b78d08
Ed Kolis <edkolis@gmail.com>
2014-08-19 10:42:38 -0400

    Added system name tooltips to galaxy map

--- FrEee.WinForms/Controls/GalaxyView.Designer.cs
+++ FrEee.WinForms/Controls/GalaxyView.Designer.cs
@@ -28,9 +28,24 @@ namespace FrEee.WinForms.Controls
 		/// </summary>
 		private void InitializeComponent()
 		{
-			components = new System.ComponentModel.Container();
+			this.components = new System.ComponentModel.Container();
+			this.toolTip = new System.Windows.Forms.ToolTip(this.components);
+			this.SuspendLayout();
+			// 
+			// toolTip
+			// 
+			this.toolTip.AutomaticDelay = 10;
+			this.toolTip.AutoPopDelay = 9999999;
+			this.toolTip.InitialDelay = 10;
+			this.toolTip.IsBalloon = true;
+			this.toolTip.ReshowDelay = 99999999;
+			this.toolTip.UseAnimation = false;
+			this.ResumeLayout(false);
+
 		}
 
 		#endregion
+
+		private System.Windows.Forms.ToolTip toolTip;
 	}
 }
