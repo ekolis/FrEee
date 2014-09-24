a574aa776cc20291b7fff07362d9695c3237f34c
Ed Kolis <edkolis@gmail.com>
2014-09-24 09:32:33 -0400

    Allow empires that have only planets, not just vehicles, in combat to scan enemy designs

--- FrEee/Game/Objects/Vehicles/Design.cs
+++ FrEee/Game/Objects/Vehicles/Design.cs
@@ -521,7 +521,7 @@ namespace FrEee.Game.Objects.Vehicles
 			// do we already know the design? or did we engage in combat with it this turn?
 			// TODO - "battle manager" so we're not tied to a specific combat implementation
 			else if (emp.KnownDesigns.Contains(this) || Galaxy.Current.Battles.Any(b =>
-				b.StartCombatants.Values.OfType<IVehicle>().Any(v => v.Owner == emp) &&
+				b.StartCombatants.Values.Any(c => c.Owner == emp) &&
 				b.StartCombatants.Values.OfType<IVehicle>().Any(v => v.Design == this)))
 				return Visibility.Scanned;
 			return Visibility.Unknown;
