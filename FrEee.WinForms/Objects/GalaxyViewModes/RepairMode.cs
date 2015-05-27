77c2d70e36d64feaf50aa4868698ccef5cb3c2d2
Ed Kolis <edkolis@gmail.com>
2015-05-27 14:21:43 -0400

    Added repair mode

---
+++ FrEee.WinForms/Objects/GalaxyViewModes/RepairMode.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using FrEee.Game.Interfaces;
+using FrEee.Game.Objects.Civilization;
+using FrEee.Game.Objects.Space;
+using FrEee.Utility;
+using FrEee.Utility.Extensions;
+
+namespace FrEee.WinForms.Objects.GalaxyViewModes
+{
+	/// <summary>
+	/// Displays repair rates in each resource.
+	/// </summary>
+	public class RepairMode : ArgbMode
+	{
+		public override string Name
+		{
+			get { return "Repair"; }
+		}
+
+		protected override Color GetColor(StarSystem sys)
+		{
+			var max = Galaxy.Current.StarSystemLocations.Max(l => GetRepair(l.Item));
+			if (max == 0)
+				return Color.Black;
+			var sat = Weight(GetRepair(sys), max);
+			return Color.FromArgb(sat, sat, sat);
+		}
+
+		private int GetRepair(StarSystem sys)
+		{
+			return sys.FindSpaceObjects<ISpaceObject>().OwnedBy(Empire.Current).Sum(x => x.GetAbilityValue("Component Repair").ToInt());
+		}
+	}
+}
