45fa5ded6c95c73df6f6726ee223510c2d9ec9ca
Ed Kolis <edkolis@gmail.com>
2015-05-27 14:14:17 -0400

    Added construction rate galaxy view mode.

---
+++ FrEee.WinForms/Objects/GalaxyViewModes/ConstructionMode.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using FrEee.Game.Interfaces;
+using FrEee.Game.Objects.Civilization;
+using FrEee.Game.Objects.Space;
+using FrEee.Utility;
+using FrEee.Utility.Extensions;
+
+namespace FrEee.WinForms.Objects.GalaxyViewModes
+{
+	/// <summary>
+	/// Displays space yards by build rate in each resource.
+	/// </summary>
+	public class ConstructionMode : ArgbMode
+	{
+		public override string Name
+		{
+			get { return "Construction"; }
+		}
+
+		protected override Color GetColor(StarSystem sys)
+		{
+			var max = Galaxy.Current.StarSystemLocations.MaxOfAllResources(l => GetSY(l.Item));
+			foreach (var r in Resource.All)
+				if (max[r] == 0)
+					max[r] = int.MaxValue; // prevent divide by zero
+			var amt = GetSY(sys);
+			var blue = 255 * amt[Resource.Minerals] / max[Resource.Minerals];
+			var green = 255 * amt[Resource.Organics] / max[Resource.Organics];
+			var red = 255 * amt[Resource.Radioactives] / max[Resource.Radioactives];
+			return Color.FromArgb(red, green, blue);
+		}
+
+		private ResourceQuantity GetSY(StarSystem sys)
+		{
+			var qs = sys.FindSpaceObjects<ISpaceObject>().OwnedBy(Empire.Current).Select(x => x.ConstructionQueue).Where(q => q != null);
+			return qs.Sum(q => q.Rate);
+		}
+	}
+}
