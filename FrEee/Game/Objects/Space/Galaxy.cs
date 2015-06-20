b26a4f09ebb6f45b8d96d3d4dbf20a653e4358ab
Ed Kolis <edkolis@gmail.com>
2015-06-20 14:44:43 -0400

    Designs are imported on load

--- FrEee/Game/Objects/Space/Galaxy.cs
+++ FrEee/Game/Objects/Space/Galaxy.cs
@@ -520,6 +520,14 @@ namespace FrEee.Game.Objects.Space
 
 				// load library of designs, strategies, etc.
 				Library.Load();
+
+				// import designs
+				Library.Import<IDesign>().Where(d => !Empire.Current.KnownDesigns.Any(d2 => d2.Name == d.Name)).SafeForeach(d =>
+					{
+						d.Owner = Empire.Current;
+						d.TurnNumber = Galaxy.Current.TurnNumber;
+						Empire.Current.KnownDesigns.Add(d); // only client side, don't need to worry about other players spying :)
+					});
 			}
 		}
 
