876fffba20a154c7b46fbfbf50dc649e146a6db5
Ed Kolis <edkolis@gmail.com>
2015-06-22 16:44:52 -0400

    Added commands to set and clear player notes (though right now nothing in the GUI calls them).

---
+++ FrEee/Game/Objects/Commands/ClearPlayerNoteCommand.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using FrEee.Game.Interfaces;
+using FrEee.Game.Objects.Civilization;
+using FrEee.Utility;
+using FrEee.Utility.Extensions;
+
+namespace FrEee.Game.Objects.Commands
+{
+	/// <summary>
+	/// Sets a player note.
+	/// </summary>
+	public class SetPlayerNoteCommand : Command<Empire>
+	{
+		public SetPlayerNoteCommand(IReferrable target, string note)
+			: base(Empire.Current)
+		{
+			Target = target;
+			Note = note;
+		}
+
+		[DoNotSerialize]
+		public IReferrable Target { get { return target.Value; } set { target = value.ReferViaGalaxy(); } }
+
+		private GalaxyReference<IReferrable> target { get; set; }
+
+		public string Note { get; set; }
+
+		public override void Execute()
+		{
+			Executor.PlayerNotes[target] = Note;
+		}
+	}
+}
---
+++ FrEee/Game/Objects/Commands/SetPlayerNoteCommand.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using FrEee.Game.Interfaces;
+using FrEee.Game.Objects.Civilization;
+using FrEee.Utility;
+using FrEee.Utility.Extensions;
+
+namespace FrEee.Game.Objects.Commands
+{
+	/// <summary>
+	/// Clears a player note.
+	/// </summary>
+	public class ClearPlayerNoteCommand : Command<Empire>
+	{
+		public ClearPlayerNoteCommand(IReferrable target)
+			: base(Empire.Current)
+		{
+			Target = target;
+		}
+
+		[DoNotSerialize]
+		public IReferrable Target { get { return target.Value; } set { target = value.ReferViaGalaxy(); } }
+
+		private GalaxyReference<IReferrable> target { get; set; }
+
+		public override void Execute()
+		{
+			Executor.PlayerNotes.Remove(target);
+		}
+	}
+}
