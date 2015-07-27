ebd0606da9b4768a5105154aa72627c15f0a4e19
ProgrammerHero <ProgrammerHero@localhost>
2015-07-27 15:08:24 -0300

    fix flickering progressbar on StatsForm

--- FrEee.WinForms/Forms/StatusForm.cs
+++ FrEee.WinForms/Forms/StatusForm.cs
@@ -59,5 +59,13 @@ namespace FrEee.WinForms.Forms
 					Close();
 			}
 		}
-	}
+
+    protected override CreateParams CreateParams {
+      get {
+        CreateParams cp = base.CreateParams;
+        cp.ExStyle |= 0x02000000;
+        return cp;
+      }
+    }
+  }
 }
