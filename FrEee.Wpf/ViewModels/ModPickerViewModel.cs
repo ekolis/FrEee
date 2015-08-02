6bf947b5e34fd98b35c2bb2a2102e30deb0d0fef
Ed Kolis <edkolis@gmail.com>
2015-08-01 20:18:30 -0400

    Added mod picker view to WPF app. You will now need the DynamicViewModel library to build the WPF app. Since it's still in development and I don't have a nuget server, you will need to clone it from https://bitbucket.org/ekolis/dynamicviewmodel and set it up like so: Projects/FrEee/lib/DynamicViewModel

---
+++ FrEee.Wpf/ViewModels/ModPickerViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.ObjectModel;
+using DynamicViewModel;
+using FrEee.Modding;
+
+namespace FrEee.Wpf.ViewModels
+{
+	/// <summary>
+	/// View model for the mod picker.
+	/// Allows the user to choose a mod from a list of mods.
+	/// </summary>
+	public class ModPickerViewModel : DynamicSelectorViewModel<ObservableCollection<ModInfo>, ModInfo>
+	{
+		public ModPickerViewModel()
+			: base(typeof(ObservableCollection<ModInfo>))
+		{
+
+		}
+	}
+}
--- FrEee.Wpf/Views/MainMenuView.xaml.cs
+++ FrEee.Wpf/Views/MainMenuView.xaml.cs
@@ -9,5 +9,45 @@
 		{
 			InitializeComponent();
 		}
+
+		private void btnMods_Click(object sender, System.Windows.RoutedEventArgs e)
+		{
+			new ModPickerView().ShowDialog();
+		}
+
+		private void btnScenarios_Click(object sender, System.Windows.RoutedEventArgs e)
+		{
+
+		}
+
+		private void btnOptions_Click(object sender, System.Windows.RoutedEventArgs e)
+		{
+
+		}
+
+		private void btnCredits_Click(object sender, System.Windows.RoutedEventArgs e)
+		{
+
+		}
+
+		private void btnNew_Click(object sender, System.Windows.RoutedEventArgs e)
+		{
+
+		}
+
+		private void btnQuickstart_Click(object sender, System.Windows.RoutedEventArgs e)
+		{
+
+		}
+
+		private void btnResume_Click(object sender, System.Windows.RoutedEventArgs e)
+		{
+
+		}
+
+		private void btnLoad_Click(object sender, System.Windows.RoutedEventArgs e)
+		{
+
+		}
 	}
 }
