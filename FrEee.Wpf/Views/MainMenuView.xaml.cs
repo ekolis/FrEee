b24474a3e2b655cf182390f887226cb352bed2d4
Ed Kolis <edkolis@gmail.com>
2015-08-03 10:23:01 -0400

    Credits for WPF gui

--- FrEee.Wpf/Views/MainMenuView.xaml.cs
+++ FrEee.Wpf/Views/MainMenuView.xaml.cs
@@ -1,4 +1,8 @@
-﻿namespace FrEee.Wpf.Views
+﻿using System.IO;
+using System.Reflection;
+using System.Windows;
+
+namespace FrEee.Wpf.Views
 {
 	/// <summary>
 	/// Interaction logic for MainMenuView.xaml
@@ -17,37 +21,53 @@
 
 		private void btnScenarios_Click(object sender, System.Windows.RoutedEventArgs e)
 		{
-
+			// TODO - scenarios, however those will work...
+			MessageBox.Show("Sorry, the scenario feature is not yet enabled.");
 		}
 
 		private void btnOptions_Click(object sender, System.Windows.RoutedEventArgs e)
 		{
-
+			// TODO - options
+			MessageBox.Show("Sorry, the options feature is not yet enabled.");
 		}
 
 		private void btnCredits_Click(object sender, System.Windows.RoutedEventArgs e)
 		{
-
+			// TODO - proper credits view with scrolling and blackjack and hookers
+			string credits;
+			try
+			{
+				credits = File.ReadAllText("licenses/credits.txt");
+			}
+			catch
+			{
+				credits = "Could not open licenses/credits.txt.";
+			}
+			MessageBox.Show(credits, "FrEee v" + Assembly.GetExecutingAssembly().GetName().Version.ToString(3));
 		}
 
 		private void btnNew_Click(object sender, System.Windows.RoutedEventArgs e)
 		{
-
+			// TODO - start a new game with full fledged game setup and such
+			MessageBox.Show("Sorry, the new game feature is not yet enabled.");
 		}
 
 		private void btnQuickstart_Click(object sender, System.Windows.RoutedEventArgs e)
 		{
-
+			// TODO - quickstart
+			MessageBox.Show("Sorry, the quickstart feature is not yet enabled.");
 		}
 
 		private void btnResume_Click(object sender, System.Windows.RoutedEventArgs e)
 		{
-
+			// TODO - resume game
+			MessageBox.Show("Sorry, the resume game feature is not yet enabled.");
 		}
 
 		private void btnLoad_Click(object sender, System.Windows.RoutedEventArgs e)
 		{
-
+			// TODO - load game
+			MessageBox.Show("Sorry, the load game feature is not yet enabled.");
 		}
 	}
 }
