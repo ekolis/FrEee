58fdb1a1fc646c5c8a3a521962035d62206aad27
Ed Kolis <edkolis@gmail.com>
2015-08-04 16:55:44 -0400

    Forgot about non-empire-specific modifiers

--- FrEee/Game/Objects/Vehicles/Vehicle.cs
+++ FrEee/Game/Objects/Vehicles/Vehicle.cs
@@ -412,7 +412,9 @@ namespace FrEee.Game.Objects.Vehicles
 					this.GetAbilityValue("Combat To Hit Offense Plus").ToInt()
 					- this.GetAbilityValue("Combat To Hit Offense Minus").ToInt()
 					+ Owner.Culture.SpaceCombat
+					+ Sector.GetAbilityValue("Combat Modifier - Sector").ToInt()
 					+ Sector.GetAbilityValue(Owner, "Combat Modifier - Sector").ToInt()
+					+ StarSystem.GetAbilityValue("Combat Modifier - System").ToInt()
 					+ StarSystem.GetAbilityValue(Owner, "Combat Modifier - System").ToInt()
 					+ Owner.GetAbilityValue("Combat Modifier - Empire").ToInt();
 			}
@@ -426,7 +428,9 @@ namespace FrEee.Game.Objects.Vehicles
 					this.GetAbilityValue("Combat To Hit Defense Plus").ToInt()
 					- this.GetAbilityValue("Combat To Hit Defense Minus").ToInt()
 					+ Owner.Culture.SpaceCombat
+					+ Sector.GetAbilityValue("Combat Modifier - Sector").ToInt()
 					+ Sector.GetAbilityValue(Owner, "Combat Modifier - Sector").ToInt()
+					+ StarSystem.GetAbilityValue("Combat Modifier - System").ToInt()
 					+ StarSystem.GetAbilityValue(Owner, "Combat Modifier - System").ToInt()
 					+ Owner.GetAbilityValue("Combat Modifier - Empire").ToInt();
 			}
