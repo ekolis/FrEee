c71c026e5cb50b45f6a11326a3fc67f1b5a36d2b
Ed Kolis <edkolis@gmail.com>
2015-08-05 19:41:48 -0400

    Added ClientSideCache<T> class (untested)

---
+++ FrEee/Utility/ClientSideCache.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using FrEee.Game.Objects.Civilization;
+
+namespace FrEee.Utility
+{
+	/// <summary>
+	/// A client side cache for data.
+	/// Can also cache data server side when a flag is enabled.
+	/// </summary>
+	/// <typeparam name="T"></typeparam>
+	public class ClientSideCache<T>
+	{
+		public ClientSideCache(Func<T> compute)
+		{
+			this.compute = compute;
+		}
+
+		private Func<T> compute;
+
+		private bool isServerSideCacheEnabled;
+
+		public bool IsServerSideCacheEnabled
+		{
+			get
+			{
+				return isServerSideCacheEnabled;
+			}
+			set
+			{
+				isServerSideCacheEnabled = value;
+				if (!isServerSideCacheEnabled)
+					isDirty = true;
+			}
+		}
+
+		public bool IsCacheEnabled
+		{
+			get
+			{
+				return IsServerSideCacheEnabled || Empire.Current != null;
+			}
+		}
+
+		private bool isDirty = true;
+
+		private T value;
+
+		public T Value
+		{
+			get
+			{
+				if (IsCacheEnabled)
+				{
+					if (!isDirty)
+						return value;
+					else
+					{
+						value = compute();
+						isDirty = false;
+						return value;
+					}
+				}
+				else
+					return compute();
+			}
+		}
+
+		public static implicit operator T(ClientSideCache<T> cache)
+		{
+			return cache.Value;
+		}
+	}
+}
