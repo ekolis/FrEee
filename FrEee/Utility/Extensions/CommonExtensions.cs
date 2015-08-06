9cef84fafb94cb5f9226348f32804c8e105953cf
Ed Kolis <edkolis@gmail.com>
2015-08-05 22:23:07 -0400

    No need to check for abilities on null objects anyway

--- FrEee/Utility/Extensions/CommonExtensions.cs
+++ FrEee/Utility/Extensions/CommonExtensions.cs
@@ -2782,6 +2782,9 @@ namespace FrEee.Utility.Extensions
 		/// <returns></returns>
 		public static IEnumerable<Ability> UnstackedAbilities(this IAbilityObject obj, Func<IAbilityObject, bool> sourceFilter = null)
 		{
+			if (obj == null)
+				return Enumerable.Empty<Ability>();
+
 			// TODO - should we include shared abilities here? HasAbility has a flag to include these...
 			if (sourceFilter == null || sourceFilter(obj))
 				return obj.IntrinsicAbilities.Concat(obj.SharedAbilities(sourceFilter)).Concat(obj.DescendantAbilities(sourceFilter)).Concat(obj.AncestorAbilities(sourceFilter));
