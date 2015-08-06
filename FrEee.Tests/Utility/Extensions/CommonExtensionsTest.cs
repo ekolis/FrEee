cddd0f61eb05daadc5202162dfb8f59ecd8480c8
Ed Kolis <edkolis@gmail.com>
2015-08-06 08:56:46 -0400

    Added SpawnTasksAsync method. This method can be used to spawn a bunch of tasks to do various things. It completes when all the things are done. Maybe we need one that keeps track of incremental progress?

---
+++ FrEee.Tests/Utility/Extensions/CommonExtensionsTest.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using FrEee.Utility.Extensions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace FrEee.Tests.Utility.Extensions
+{
+	/// <summary>
+	/// Tests extension methods.
+	/// </summary>
+	[TestClass]
+	public class CommonExtensionsTest
+	{
+		[TestMethod]
+		public void SpawnTasksAsync()
+		{
+			var nums = new List<int>();
+			for (var i = 0; i <= 5; i++)
+				nums.Add(i);
+			var squares = nums.SpawnTasksAsync(i => new Square(i)).Result;
+			for (var i = 0; i <= 5; i++)
+				Assert.IsTrue(squares.Single(s => s.Number == i).SquaredNumber == i * i);
+			Assert.IsTrue(!squares.Any(s => s.Number < 0));
+			Assert.IsTrue(!squares.Any(s => s.Number > 5));
+		}
+
+		private class Square
+		{
+			public Square(int i)
+			{
+				Number = i;
+			}
+
+			public int Number { get; private set; }
+
+			public int SquaredNumber { get { return Number * Number; } }
+		}
+	}
+}
