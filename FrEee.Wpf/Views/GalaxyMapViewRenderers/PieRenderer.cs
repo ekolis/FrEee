f0283ee60cddfcee35aa35ebae4414f1fd084f8c
Ed Kolis <edkolis@gmail.com>
2015-09-02 18:03:25 -0400

    Fixed drawing of system circles.

--- FrEee.Wpf/Views/GalaxyMapViewRenderers/PieRenderer.cs
+++ FrEee.Wpf/Views/GalaxyMapViewRenderers/PieRenderer.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Media;
 using FrEee.Game.Objects.Space;
+using FrEee.Wpf.Utility;
 using FrEee.Wpf.Views.Shapes;
 
 namespace FrEee.Wpf.Views.GalaxyMapViewRenderers
@@ -39,12 +40,12 @@ namespace FrEee.Wpf.Views.GalaxyMapViewRenderers
 					var arc = 360d * amount / amountHere;
 					var brush = new SolidColorBrush(Color.FromArgb(GetAlpha(sys), color.R, color.G, color.B));
 					var penBrush = new SolidColorBrush(color);
-					var drawing = new GeometryDrawing(brush, new Pen(brush, 1), new PieSlice
+					var pieSlice = new PieSlice
 					{
 						StartDegrees = curAngle,
 						DeltaDegrees = arc,
-                    }.RenderedGeometry);
-					dc.DrawDrawing(drawing);
+					};
+					dc.DrawGeometry(brush, new Pen(penBrush, 1), CreateGeometry(p, radius, curAngle, arc));
 					curAngle += arc;
 				}
 			}
@@ -65,6 +66,43 @@ namespace FrEee.Wpf.Views.GalaxyMapViewRenderers
 		protected byte WeightAlpha(double amount, double max)
 		{
 			return (byte)(255 * amount / max);
+		}
+
+		private Geometry CreateGeometry(Point p, double radius, double startDegrees, double deltaDegrees)
+		{
+			// Create a StreamGeometry for describing the shape
+			StreamGeometry geometry = new StreamGeometry();
+
+			using (StreamGeometryContext context = geometry.Open())
+			{
+				var rad1 = Deg2Rad(startDegrees);
+				var drad = Deg2Rad(deltaDegrees);
+				var rad2 = rad1 + drad;
+				var p1 = new Point((Math.Cos(rad1) + 1) * radius + p.X, -(Math.Sin(rad1) - 1) * radius + p.Y);
+				var p2 = new Point((Math.Cos(rad2) + 1) * radius + p.X, -(Math.Sin(rad2) - 1) * radius + p.Y);
+
+				context.BeginFigure(p, true, true);
+				if (deltaDegrees >= 360)
+				{
+					context.DrawGeometry(new EllipseGeometry(new Rect(p.X - radius, p.Y - radius, radius * 2, radius * 2)));
+				}
+				else if (deltaDegrees >= 0)
+				{
+					context.LineTo(p1, true, true);
+					context.ArcTo(p2, new Size(radius, radius), drad, deltaDegrees > 180, SweepDirection.Counterclockwise, true, true);
+					context.Close();
+				}
+			}
+
+			// Freeze the geometry for performance benefits
+			geometry.Freeze();
+
+			return geometry;
+		}
+
+		private double Deg2Rad(double degrees)
+		{
+			return degrees / 180d * Math.PI;
 		}
 	}
 }
