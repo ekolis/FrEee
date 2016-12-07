15071739e2790ec3ad28fe1692d2831a2e64ce24
Simonas Grei?ius <simonas.greicius@stablepoint.biz>
2016-12-07 02:12:09 -0800

    Implementing a Bitmap->ImageSource converter for WPF, so I can restore PictorialLogMessage class to its original state now.

---
+++ WpfControls/Code/BitmapToImageSourceConverter.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+
+namespace FrEee.Wpf
+{
+    /// <summary>
+    /// WinForms GUI uses Bitmap classes to display pictures, but WPF doesn't really support them. So, we need to convert them to something that WPF likes.
+    /// </summary>
+    [ValueConversion(typeof(Bitmap), typeof(ImageSource))]
+    public class BitmapToImageSourceConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            if (value is Bitmap)
+            {
+                using (MemoryStream memory = new MemoryStream())
+                {
+                    ((Bitmap)value).Save(memory, System.Drawing.Imaging.ImageFormat.Bmp);
+                    memory.Position = 0;
+                    BitmapImage bitmapimage = new BitmapImage();
+                    bitmapimage.BeginInit();
+                    bitmapimage.StreamSource = memory;
+                    bitmapimage.CacheOption = BitmapCacheOption.OnLoad;
+                    bitmapimage.EndInit();
+
+                    return bitmapimage;
+                }
+            }
+            else
+            {
+                return new BitmapImage();
+            }
+        }
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            return null;
+        }
+    }
+}
