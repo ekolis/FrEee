39c935ae291e70e4db76dcbc85732b4ffd0dd8bc
Simonas Grei?ius <simonas.greicius@stablepoint.biz>
2016-12-08 11:41:18 -0800

    WPF log now looks almost completely identical to the WinForms one. Moved WPF controls into the WinForms projects, because I need to reference some engine stuff from within, and since WPF is embedded back into ElementHost, that creates circular references.

---
+++ FrEee.WinForms/MVVM/FunnyLogEntryFreshnessConverter.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace FrEee.Wpf
+{
+    /// <summary>
+    /// Used to gray-out older log entries. Should be a better way, but will do for now.
+    /// </summary>
+    [ValueConversion(typeof(int), typeof(bool))]
+    public class FunnyLogEntryFreshnessConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            if (value is int)
+            {
+                if ((int)value - Game.Objects.Space.Galaxy.Current.TurnNumber == 0) return true;
+                else return false;
+            }
+
+            return false;
+        }
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            return null;
+        }
+    }
+}
