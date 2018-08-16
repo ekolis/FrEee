0adc5f3526447c72ee740523a8b0dd1f279ab52d
ekolis <ekolis@localhost>
2018-08-16 12:45:54 -0400

    Added unit test for starting a quickstart game.

---
+++ FrEee.Tests/Game/Setup/GameSetupTest.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using FrEee.Game.Objects.Space;
+using FrEee.Game.Setup;
+using FrEee.Modding;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace FrEee.Tests.Game.Setup
+{
+	[TestClass]
+	public class GameSetupTest
+	{
+		[TestMethod]
+		public void Quickstart()
+		{
+			TestUtilities.SetEntryAssembly();
+			var setup = GameSetup.Load(@"..\..\..\..\FrEee\GameSetups\quickstart.gsu");
+			Mod.Current = Mod.Load(null);
+			Galaxy.Initialize(setup);
+		}
+	}
+}
