df7ffd02c8fe01db6182ee3803aa3b815b33ca02
Narf the Mouse <Narf the Mouse@localhost>
2018-08-18 18:29:24 -0700

    Fixed: DataFile now correctly parses nested directories, passes tests.

--- FrEee/Modding/DataFile.cs
+++ FrEee/Modding/DataFile.cs
@@ -7,66 +7,69 @@ using System.Text;
 
 namespace FrEee.Modding
 {
-	/// <summary>
-	/// A data file which is part of a mod.
-	/// </summary>
-	 [Serializable] public class DataFile
-	{
-		 /// <summary>
-		 /// Loads a data file from disk.
-		 /// </summary>
-		 /// <param name="modpath">The mod path, relative to the FrEee/Mods folder, or null to use the stock mod.</param>
-		 /// <param name="filename">The file name relative to the mod's Data folder.</param>
-		 /// <returns></returns>
-		 public static DataFile Load(string modpath, string filename)
-		 {
-			 // TODO - fall back on stock data when mod data not found
-			 var datapath = modpath == null ? "Data" : Path.Combine("Mods", modpath, "Data");
-                   
-             var filepath = Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), datapath, filename);
-			 if (File.Exists(filepath))
-				 return new DataFile(File.ReadAllText(filepath));
-			 // got here? then try the stock data file instead if we were loading a mod
-			 if (modpath != null)
-			 {
-                 filepath = Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), "Data", filename);
-				 if (File.Exists(filepath))
-					 return new DataFile(File.ReadAllText(filepath));
-			 }
-			 // got here? then data file was not found even in stock
-			 throw new FileNotFoundException("Could not find data file: " + filename + ".", filename);
-		 }
+    /// <summary>
+    /// A data file which is part of a mod.
+    /// </summary>
+    [Serializable]
+    public class DataFile
+    {
+        /// <summary>
+        /// Loads a data file from disk.
+        /// </summary>
+        /// <param name="modpath">The mod path, relative to the FrEee/Mods folder, or null to use the stock mod.</param>
+        /// <param name="filename">The file name relative to the mod's Data folder.</param>
+        /// <returns></returns>
+        public static DataFile Load(string modpath, string filename, string subpath = "")
+        {
+            // TODO - fall back on stock data when mod data not found
+            var datapath = modpath == null ? "Data" : Path.Combine("Mods", modpath, "Data", subpath);
 
-		/// <summary>
-		/// Creates a data file with no records
-		/// </summary>
-		public DataFile()
-		{
-			MetaRecords = new List<MetaRecord>();
-		}
+            DataPath =  modpath;
 
-		/// <summary>
-		/// Creates a data file by parsing some string data.
-		/// </summary>
-		/// <param name="data"></param>
-		public DataFile(string data)
-			: this()
-		{
-			// split data into lines
-			var lines = data.Split(new string[] { "\n" }, StringSplitOptions.RemoveEmptyEntries).Select(l => l.Trim()).ToArray();
+            var filepath = Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), datapath, filename);
+            if (File.Exists(filepath))
+                return new DataFile(File.ReadAllText(filepath));
+            // got here? then try the stock data file instead if we were loading a mod
+            if (modpath != null)
+            {
+                filepath = Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), "Data", filename);
+                if (File.Exists(filepath))
+                    return new DataFile(File.ReadAllText(filepath));
+            }
+            // got here? then data file was not found even in stock
+            throw new FileNotFoundException($"Could not find data file: {filename} at {filepath}.", filename);
+        }
 
-			int curLine;
+        /// <summary>
+        /// Creates a data file with no records
+        /// </summary>
+        public DataFile()
+        {
+            MetaRecords = new List<MetaRecord>();
+        }
 
-			// find *BEGIN* tag
-			// TODO - if no *BEGIN* tag present, don't require it
-			for (curLine = 0; curLine < lines.Length; curLine++)
-			{
-				if (lines[curLine] == "*BEGIN*")
-					break;
-			}
+        /// <summary>
+        /// Creates a data file by parsing some string data.
+        /// </summary>
+        /// <param name="data"></param>
+        public DataFile(string data)
+            : this()
+        {
+            // split data into lines
+            var lines = data.Split(new string[] { "\n" }, StringSplitOptions.RemoveEmptyEntries).Select(l => l.Trim()).ToArray();
 
-			// skip 2 lines
-			curLine += 2;
+            int curLine;
+
+            // find *BEGIN* tag
+            // TODO - if no *BEGIN* tag present, don't require it
+            for (curLine = 0; curLine < lines.Length; curLine++)
+            {
+                if (lines[curLine] == "*BEGIN*")
+                    break;
+            }
+
+            // skip 2 lines
+            curLine += 2;
 
             List<DataFile> includeDataFiles = new List<DataFile>();
             while (lines[curLine].StartsWith("#include") ||
@@ -82,45 +85,41 @@ namespace FrEee.Modding
                 int firstPQ = line.IndexOf('"') + 1;
                 int lastPQ = line.LastIndexOf('"') - 1;
                 string includePath = line.Substring(firstPQ, (lastPQ - firstPQ) + 1);
-                if (Path.IsPathRooted(includePath) == false)
-                {
-                    includePath = Path.Combine(Directory.GetCurrentDirectory(), includePath);
-                }
                 string filename = Path.GetFileName(includePath);
                 if (string.IsNullOrEmpty(filename))
                     throw new FileNotFoundException($"#include path \"{includePath}\" at line {curLine} does not contain filename");
-                string modpath = Path.GetDirectoryName(includePath);
-                includeDataFiles.Add(Load(modpath, filename));
+                string subpath = Path.GetDirectoryName(includePath);
+                includeDataFiles.Add(Load(DataPath, filename, subpath));
 
                 ++curLine;
             }
 
             // start reading records
             var recLines = new List<string>();
-			for (; curLine < lines.Length; curLine++)
-			{
-				if (string.IsNullOrWhiteSpace(lines[curLine]))
-				{
-					// done with a record
-					if (recLines.Count > 0)
-					{
-						MetaRecords.Add(new MetaRecord(recLines));
-						recLines.Clear();
-					}
-				}
-				else
-				{
-					// add line to current record
-					recLines.Add(lines[curLine]);
-				}
-			}
+            for (; curLine < lines.Length; curLine++)
+            {
+                if (string.IsNullOrWhiteSpace(lines[curLine]))
+                {
+                    // done with a record
+                    if (recLines.Count > 0)
+                    {
+                        MetaRecords.Add(new MetaRecord(recLines));
+                        recLines.Clear();
+                    }
+                }
+                else
+                {
+                    // add line to current record
+                    recLines.Add(lines[curLine]);
+                }
+            }
 
-			// deal with the last record
-			if (recLines.Count > 0)
-				MetaRecords.Add(new MetaRecord(recLines));
+            // deal with the last record
+            if (recLines.Count > 0)
+                MetaRecords.Add(new MetaRecord(recLines));
 
-			// deal with degenerate records
-			MetaRecords = MetaRecords.Where(rec => rec.Fields.Count > 0).ToList();
+            // deal with degenerate records
+            MetaRecords = MetaRecords.Where(rec => rec.Fields.Count > 0).ToList();
 
             List<MetaRecord> compiledRecords = new List<MetaRecord>();
             foreach (var includeDataFile in includeDataFiles)
@@ -136,26 +135,28 @@ namespace FrEee.Modding
             MetaRecords = compiledRecords;
         }
 
-		/// <summary>
-		/// The meta records in this data file.
-		/// Meta records are records that can contain parameters and static formulas.
-		/// </summary>
-		public IList<MetaRecord> MetaRecords { get; private set; }
+        /// <summary>
+        /// The meta records in this data file.
+        /// Meta records are records that can contain parameters and static formulas.
+        /// </summary>
+        public IList<MetaRecord> MetaRecords { get; private set; }
 
-		 /// <summary>
-		 /// The individual records generated from the meta records.
-		 /// </summary>
-		public IEnumerable<Record> Records
-		{
-			get
-			{
-				return MetaRecords.SelectMany(mr => mr.Instantiate());
-			}
-		}
+        /// <summary>
+        /// The individual records generated from the meta records.
+        /// </summary>
+        public IEnumerable<Record> Records
+        {
+            get
+            {
+                return MetaRecords.SelectMany(mr => mr.Instantiate());
+            }
+        }
 
-		public override string ToString()
-		{
-			return "(" + MetaRecords.Count + " meta records)";
-		}
-	}
+        public static string DataPath { get; private set; }
+
+        public override string ToString()
+        {
+            return "(" + MetaRecords.Count + " meta records)";
+        }
+    }
 }
