616caed3c0b1f4962c5fb98912a13e9e446ead6c
Narf the Mouse <Narf the Mouse@localhost>
2018-08-19 13:08:06 -0700

    Changed: Extracted UnitHandlingExtensions from Common and Conversion Extensions.

--- FrEee/Utility/Extensions/ConversionExtensions.cs
+++ FrEee/Utility/Extensions/ConversionExtensions.cs
@@ -1,38 +1,13 @@
 ﻿using FrEee.Modding.Interfaces;
 using System;
-using System.Collections.Generic;
 using System.Drawing;
 using System.Globalization;
-using System.Linq;
 using System.Text;
 
 namespace FrEee.Utility.Extensions
 {
     public static class ConversionExtensions
     {
-        #region Private Fields
-
-        private static IDictionary<int, string> RomanNumeralCache = new Dictionary<int, string>();
-
-        private static Tuple<int, string>[] RomanNumeralParts = new Tuple<int, string>[]
-                        {
-            Tuple.Create(1000, "M"),
-            Tuple.Create(900, "CM"),
-            Tuple.Create(500, "D"),
-            Tuple.Create(400, "CD"),
-            Tuple.Create(100, "C"),
-            Tuple.Create(90, "XC"),
-            Tuple.Create(50, "L"),
-            Tuple.Create(40, "XL"),
-            Tuple.Create(10, "X"),
-            Tuple.Create(9, "IX"),
-            Tuple.Create(5, "V"),
-            Tuple.Create(4, "IV"),
-            Tuple.Create(1, "I"),
-                        };
-
-        #endregion Private Fields
-
         #region Public Methods
 
         /// <summary>
@@ -171,38 +146,6 @@ namespace FrEee.Utility.Extensions
             return new Point(v.X, v.Y);
         }
 
-        /// <summary>
-        /// Gets a roman numeral.
-        /// </summary>
-        /// <param name="i"></param>
-        /// <returns></returns>
-        public static string ToRomanNumeral(this int i)
-        {
-            // do we already know this?
-            if (!RomanNumeralCache.ContainsKey(i))
-            {
-                // get silly negative numbers and zeroes out of the way
-                if (i < 0)
-                    RomanNumeralCache.Add(i, "-" + ToRomanNumeral(-i));
-                else if (i == 0)
-                    RomanNumeralCache.Add(i, "");
-                else
-                {
-                    // scan the roman numeral parts list recursively
-                    foreach (var part in RomanNumeralParts.OrderByDescending(part => part.Item1))
-                    {
-                        if (i >= part.Item1)
-                        {
-                            RomanNumeralCache.Add(i, part.Item2 + (i - part.Item1).ToRomanNumeral());
-                            break;
-                        }
-                    }
-                }
-            }
-
-            return RomanNumeralCache[i];
-        }
-
         public static string ToSafeString(this object o)
         {
             if (o == null)
@@ -229,17 +172,6 @@ namespace FrEee.Utility.Extensions
             return sb.ToString();
         }
 
-        /// <summary>
-        /// Converts a turn number to a stardate.
-        /// </summary>
-        /// <param name="turnNumber"></param>
-        /// <returns></returns>
-        public static string ToStardate(this int turnNumber)
-        {
-            // TODO - moddable starting stardate?
-            return ((turnNumber + 23999) / 10.0).ToString("0.0");
-        }
-
         public static string ToString(this double? d, string fmt)
         {
             if (d == null)
@@ -255,118 +187,6 @@ namespace FrEee.Utility.Extensions
         public static string ToStringInvariant(this IConvertible c)
         {
             return (string)Convert.ChangeType(c, typeof(string), CultureInfo.InvariantCulture);
-        }
-
-        /// <summary>
-        /// Adds SI prefixes to a value and rounds it off.
-        /// e.g. 25000 becomes 25.00k
-        /// </summary>
-        /// <param name="value"></param>
-        public static string ToUnitString(this long? value, bool bForBillions = false, int sigfigs = 4, string undefinedValue = "Undefined")
-        {
-            if (value == null)
-                return undefinedValue;
-            return value.Value.ToUnitString(bForBillions, sigfigs);
-        }
-
-        /// <summary>
-        /// Adds SI prefixes to a value and rounds it off.
-        /// e.g. 25000 becomes 25.00k
-        /// </summary>
-        /// <param name="value"></param>
-        public static string ToUnitString(this long value, bool bForBillions = false, int sigfigs = 4)
-        {
-            if (Math.Abs(value) >= 1e12 * Math.Pow(10, sigfigs - 3))
-            {
-                var log = (int)Math.Floor(Math.Log10(Math.Abs(value) / 1e12));
-                var decimals = sigfigs - 1 - log;
-                return (value / 1e12).ToString("f" + decimals) + "T";
-            }
-            if (Math.Abs(value) >= 1e9 * Math.Pow(10, sigfigs - 3))
-            {
-                var log = (int)Math.Floor(Math.Log10(Math.Abs(value) / 1e9));
-                var decimals = sigfigs - 1 - log;
-                return (value / 1e9).ToString("f" + decimals) + (bForBillions ? "B" : "G");
-            }
-            if (Math.Abs(value) >= 1e6 * Math.Pow(10, sigfigs - 3))
-            {
-                var log = (int)Math.Floor(Math.Log10(Math.Abs(value) / 1e6));
-                var decimals = sigfigs - 1 - log;
-                return (value / 1e6).ToString("f" + decimals) + "M";
-            }
-            if (Math.Abs(value) >= 1e3 * Math.Pow(10, sigfigs - 3))
-            {
-                var log = (int)Math.Floor(Math.Log10(Math.Abs(value) / 1e3));
-                var decimals = sigfigs - 1 - log;
-                return (value / 1e3).ToString("f" + decimals) + "k";
-            }
-            return value.ToString();
-        }
-
-        /// <summary>
-        /// Adds SI prefixes to a value and rounds it off.
-        /// e.g. 25000 becomes 25.00k
-        /// </summary>
-        /// <param name="value"></param>
-        public static string ToUnitString(this int? value, bool bForBillions = false, int sigfigs = 4)
-        {
-            return ((long?)value).ToUnitString(bForBillions, sigfigs);
-        }
-
-        /// <summary>
-        /// Adds SI prefixes to a value and rounds it off.
-        /// e.g. 25000 becomes 25.00k
-        /// </summary>
-        /// <param name="value"></param>
-        public static string ToUnitString(this int value, bool bForBillions = false, int sigfigs = 4)
-        {
-            return ((long)value).ToUnitString(bForBillions, sigfigs);
-        }
-
-        /// <summary>
-        /// Adds SI prefixes to a value and rounds it off.
-        /// e.g. 25000 becomes 25.00k
-        /// </summary>
-        /// <param name="value"></param>
-        public static string ToUnitString(this double value, bool bForBillions = false, int sigfigs = 4)
-        {
-            if (Math.Abs(value) >= 1e12 * Math.Pow(10, sigfigs - 3))
-            {
-                var log = (int)Math.Floor(Math.Log10(Math.Abs(value) / 1e12));
-                var decimals = sigfigs - 1 - log;
-                return (value / 1e12).ToString("f" + decimals) + "T";
-            }
-            if (Math.Abs(value) >= 1e9 * Math.Pow(10, sigfigs - 3))
-            {
-                var log = (int)Math.Floor(Math.Log10(Math.Abs(value) / 1e9));
-                var decimals = sigfigs - 1 - log;
-                return (value / 1e9).ToString("f" + decimals) + (bForBillions ? "B" : "G");
-            }
-            if (Math.Abs(value) >= 1e6 * Math.Pow(10, sigfigs - 3))
-            {
-                var log = (int)Math.Floor(Math.Log10(Math.Abs(value) / 1e6));
-                var decimals = sigfigs - 1 - log;
-                return (value / 1e6).ToString("f" + decimals) + "M";
-            }
-            if (Math.Abs(value) >= 1e3 * Math.Pow(10, sigfigs - 3))
-            {
-                var log = (int)Math.Floor(Math.Log10(Math.Abs(value) / 1e3));
-                var decimals = sigfigs - 1 - log;
-                return (value / 1e3).ToString("f" + decimals) + "k";
-            }
-            return value.ToString();
-        }
-
-        /// <summary>
-        /// Adds SI prefixes to a value and rounds it off.
-        /// e.g. 25000 becomes 25.00k
-        /// </summary>
-        /// <param name="value"></param>
-        public static string ToUnitString(this double? value, bool bForBillions = false, int sigfigs = 4, string undefinedValue = "Undefined")
-        {
-            if (value == null)
-                return undefinedValue;
-            return value.Value.ToUnitString(bForBillions, sigfigs);
         }
 
         #endregion Public Methods
