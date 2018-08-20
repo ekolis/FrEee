b94d48934034e3e8831a7e27b6c57fd2611579ef
ekolis <ekolis@localhost>
2018-08-20 14:28:13 -0400

    Added clickable minimap to battle replays.

--- FrEee.WinForms/Forms/BattleReplayForm.Designer.cs
+++ FrEee.WinForms/Forms/BattleReplayForm.Designer.cs
@@ -28,10 +28,42 @@
 		/// </summary>
 		private void InitializeComponent()
 		{
-			this.btnClose = new FrEee.WinForms.Controls.GameButton();
 			this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
-			this.battleView1 = new FrEee.WinForms.Controls.BattleView();
+			this.minimap = new FrEee.WinForms.Controls.BattleView();
+			this.battleView = new FrEee.WinForms.Controls.BattleView();
+			this.btnClose = new FrEee.WinForms.Controls.GameButton();
 			this.SuspendLayout();
+			// 
+			// minimap
+			// 
+			this.minimap.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+			this.minimap.AutoZoom = true;
+			this.minimap.BackColor = System.Drawing.Color.Black;
+			this.minimap.Battle = null;
+			this.minimap.FocusedLocation = null;
+			this.minimap.Location = new System.Drawing.Point(749, 12);
+			this.minimap.Name = "minimap";
+			this.minimap.Size = new System.Drawing.Size(256, 256);
+			this.minimap.TabIndex = 28;
+			this.minimap.Text = "minimap";
+			this.minimap.UseSquares = true;
+			this.minimap.MouseDown += new System.Windows.Forms.MouseEventHandler(this.minimap_MouseDown);
+			// 
+			// battleView
+			// 
+			this.battleView.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+			this.battleView.AutoZoom = false;
+			this.battleView.BackColor = System.Drawing.Color.Black;
+			this.battleView.Battle = null;
+			this.battleView.FocusedLocation = null;
+			this.battleView.Location = new System.Drawing.Point(12, 12);
+			this.battleView.Name = "battleView";
+			this.battleView.Size = new System.Drawing.Size(731, 709);
+			this.battleView.TabIndex = 27;
+			this.battleView.Text = "battleView";
+			this.battleView.UseSquares = false;
 			// 
 			// btnClose
 			// 
@@ -46,25 +78,14 @@
 			this.btnClose.UseVisualStyleBackColor = false;
 			this.btnClose.Click += new System.EventHandler(this.btnClose_Click);
 			// 
-			// battleView1
-			// 
-			this.battleView1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
-            | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
-			this.battleView1.BackColor = System.Drawing.Color.Black;
-			this.battleView1.Location = new System.Drawing.Point(12, 12);
-			this.battleView1.Name = "battleView1";
-			this.battleView1.Size = new System.Drawing.Size(993, 709);
-			this.battleView1.TabIndex = 27;
-			this.battleView1.Text = "battleView1";
-			// 
 			// BattleReplayForm
 			// 
 			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
 			this.BackColor = System.Drawing.Color.Black;
 			this.ClientSize = new System.Drawing.Size(1017, 768);
-			this.Controls.Add(this.battleView1);
+			this.Controls.Add(this.minimap);
+			this.Controls.Add(this.battleView);
 			this.Controls.Add(this.btnClose);
 			this.ForeColor = System.Drawing.Color.White;
 			this.Name = "BattleReplayForm";
@@ -77,6 +98,7 @@
 		#endregion
 		private Controls.GameButton btnClose;
 		private System.Windows.Forms.ColumnHeader columnHeader1;
-		private Controls.BattleView battleView1;
+		private Controls.BattleView battleView;
+		private Controls.BattleView minimap;
 	}
 }
--- FrEee/Utility/IntVector2.cs
+++ FrEee/Utility/IntVector2.cs
@@ -3,165 +3,170 @@ using System;
 
 namespace FrEee.Utility
 {
-    /// <summary>
-    /// A two dimensional vector of integers.
-    /// </summary>
-    public class IntVector2 : Vector2<int>, IEquatable<IntVector2>
-    {
-        #region Public Constructors
+	/// <summary>
+	/// A two dimensional vector of integers.
+	/// </summary>
+	public class IntVector2 : Vector2<int>, IEquatable<IntVector2>
+	{
+		#region Public Constructors
 
-        public IntVector2()
-            : base()
-        {
-        }
+		public IntVector2()
+			: base()
+		{
+		}
 
-        public IntVector2(int x, int y)
-            : base(x, y)
-        {
-        }
+		public IntVector2(int x, int y)
+			: base(x, y)
+		{
+		}
 
-        public IntVector2(Vector2<int> v)
-            : base(v)
-        {
-        }
+		public IntVector2(Vector2<int> v)
+			: base(v)
+		{
+		}
 
-        #endregion Public Constructors
+		#endregion Public Constructors
 
-        #region Public Properties
+		#region Public Properties
 
-        /// <summary>
-        /// Length of this vector moving in 8 directions along the grid.
-        /// </summary>
-        public int LengthEightWay
-        {
-            get
-            {
-                var dx = Math.Abs(X);
-                var dy = Math.Abs(Y);
-                return Math.Max(dx, dy);
-            }
-        }
+		/// <summary>
+		/// Length of this vector moving in 8 directions along the grid.
+		/// </summary>
+		public int LengthEightWay
+		{
+			get
+			{
+				var dx = Math.Abs(X);
+				var dy = Math.Abs(Y);
+				return Math.Max(dx, dy);
+			}
+		}
 
-        /// <summary>
-        /// Length of this vector moving only in the 4 cardinal directions.
-        /// </summary>
-        public int LengthManhattan
-        {
-            get
-            {
-                var dx = Math.Abs(X);
-                var dy = Math.Abs(Y);
-                return dx + dy;
-            }
-        }
+		/// <summary>
+		/// Length of this vector moving only in the 4 cardinal directions.
+		/// </summary>
+		public int LengthManhattan
+		{
+			get
+			{
+				var dx = Math.Abs(X);
+				var dy = Math.Abs(Y);
+				return dx + dy;
+			}
+		}
 
-        #endregion Public Properties
+		#endregion Public Properties
 
-        #region Public Methods
+		#region Public Methods
 
-        /// <summary>
-        /// Interpolates between two vectors.
-        /// </summary>
-        /// <param name="start"></param>
-        /// <param name="end"></param>
-        /// <param name="distance"></param>
-        /// <returns></returns>
-        public static IntVector2 InterpolateEightWay(IntVector2 start, IntVector2 end, int distance)
-        {
-            var trip = end - start;
-            var tripLength = trip.LengthEightWay;
-            if (distance >= tripLength)
-                return end;
-            var dx = trip.X;
-            var dy = trip.Y;
-            var pos = new IntVector2(start);
-            var remainingDistance = distance;
-            while (remainingDistance > 0)
-            {
-                if (dx == 0 && dy == 0)
-                    break;
-                if (dx != 0)
-                {
-                    pos.X += Math.Sign(dx);
-                    dx -= Math.Sign(dx);
-                }
-                if (dy != 0)
-                {
-                    pos.Y += Math.Sign(dy);
-                    dy -= Math.Sign(dy);
-                }
-                remainingDistance--;
-            }
-            return pos;
-        }
+		/// <summary>
+		/// Interpolates between two vectors.
+		/// </summary>
+		/// <param name="start"></param>
+		/// <param name="end"></param>
+		/// <param name="distance"></param>
+		/// <returns></returns>
+		public static IntVector2 InterpolateEightWay(IntVector2 start, IntVector2 end, int distance)
+		{
+			var trip = end - start;
+			var tripLength = trip.LengthEightWay;
+			if (distance >= tripLength)
+				return end;
+			var dx = trip.X;
+			var dy = trip.Y;
+			var pos = new IntVector2(start);
+			var remainingDistance = distance;
+			while (remainingDistance > 0)
+			{
+				if (dx == 0 && dy == 0)
+					break;
+				if (dx != 0)
+				{
+					pos.X += Math.Sign(dx);
+					dx -= Math.Sign(dx);
+				}
+				if (dy != 0)
+				{
+					pos.Y += Math.Sign(dy);
+					dy -= Math.Sign(dy);
+				}
+				remainingDistance--;
+			}
+			return pos;
+		}
 
-        public static IntVector2 operator -(IntVector2 v)
-        {
-            return new IntVector2(-v.X, -v.Y);
-        }
+		public static IntVector2 operator -(IntVector2 v)
+		{
+			return new IntVector2(-v.X, -v.Y);
+		}
 
-        public static IntVector2 operator -(IntVector2 v1, IntVector2 v2)
-        {
-            return v1 + -v2;
-        }
+		public static IntVector2 operator -(IntVector2 v1, IntVector2 v2)
+		{
+			return v1 + -v2;
+		}
 
-        public static bool operator !=(IntVector2 v1, IntVector2 v2)
-        {
-            return !(v1 == v2);
-        }
+		public static bool operator !=(IntVector2 v1, IntVector2 v2)
+		{
+			return !(v1 == v2);
+		}
 
-        public static IntVector2 operator *(IntVector2 v, int s)
-        {
-            return new IntVector2(v.X * s, v.Y * s);
-        }
+		public static IntVector2 operator *(IntVector2 v, int s)
+		{
+			return new IntVector2(v.X * s, v.Y * s);
+		}
 
-        public static IntVector2 operator *(int s, IntVector2 v)
-        {
-            return v * s;
-        }
+		public static IntVector2 operator /(IntVector2 v, int s)
+		{
+			return new IntVector2(v.X / s, v.Y / s);
+		}
 
-        public static IntVector2 operator +(IntVector2 v1, IntVector2 v2)
-        {
-            return new IntVector2(v1.X + v2.X, v1.Y + v2.Y);
-        }
+		public static IntVector2 operator *(int s, IntVector2 v)
+		{
+			return v * s;
+		}
 
-        public static bool operator ==(IntVector2 v1, IntVector2 v2)
-        {
-            return v1.SafeEquals(v2);
-        }
+		public static IntVector2 operator +(IntVector2 v1, IntVector2 v2)
+		{
+			return new IntVector2(v1.X + v2.X, v1.Y + v2.Y);
+		}
 
-        public int DistanceToEightWay(IntVector2 dest)
-        {
-            return (dest - this).LengthEightWay;
-        }
+		public static bool operator ==(IntVector2 v1, IntVector2 v2)
+		{
+			return v1.SafeEquals(v2);
+		}
 
-        public int DistanceToManhattan(IntVector2 dest)
-        {
-            return (dest - this).LengthManhattan;
-        }
+		public int DistanceToEightWay(IntVector2 dest)
+		{
+			return (dest - this).LengthEightWay;
+		}
 
-        public override bool Equals(object obj)
-        {
-            if (obj is IntVector2 v)
-                return this.Equals(v);
-            return false;
-        }
+		public int DistanceToManhattan(IntVector2 dest)
+		{
+			return (dest - this).LengthManhattan;
+		}
 
-        public bool Equals(IntVector2 other)
-        {
-            return X == other.X && Y == other.Y;
-        }
+		public override bool Equals(object obj)
+		{
+			if (obj is IntVector2 v)
+				return this.Equals(v);
+			return false;
+		}
 
-        public override int GetHashCode()
-        {
-            return HashCodeMasher.Mash(X, Y);
-        }
+		public bool Equals(IntVector2 other)
+		{
+			return X == other.X && Y == other.Y;
+		}
 
-        public override string ToString()
-        {
-            return $"({X}, {Y})";
-        }
+		public override int GetHashCode()
+		{
+			return HashCodeMasher.Mash(X, Y);
+		}
 
-        #endregion Public Methods
-    }
+		public override string ToString()
+		{
+			return $"({X}, {Y})";
+		}
+
+		#endregion Public Methods
+	}
 }
