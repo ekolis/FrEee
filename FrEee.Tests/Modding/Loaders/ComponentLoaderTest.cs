de5684661ae5cc96147fa359aec1ef94bf995e2c
ekolis <ekolis@localhost>
2018-08-20 17:59:56 -0400

    Fixed weapons being loaded from the mod with insane ranges.

---
+++ FrEee.Tests/Modding/Loaders/ComponentLoaderTest.cs
@@ -0,0 +1,46 @@
+﻿using FrEee.Modding;
+using FrEee.Utility.Extensions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FrEee.Tests.Modding.Loaders
+{
+	/// <summary>
+	/// Summary description for ComponentLoaderTest
+	/// </summary>
+	[TestClass]
+	public class ComponentLoaderTest
+	{
+		[TestInitialize]
+		public void Init()
+		{
+			TestUtilities.SetEntryAssembly();
+		}
+
+		/// <summary>
+		/// Makes sure standard weapons have sane values for their minimum/maximum range.
+		/// </summary>
+		[TestMethod]
+		public void WeaponRange()
+		{
+			Mod.Load("ComponentLoaderTest");
+			var w = Mod.Current.ComponentTemplates.FindByName("Range 2 Weapon");
+			Assert.AreEqual(0, w.WeaponInfo.MinRange.Value);
+			Assert.AreEqual(2, w.WeaponInfo.MaxRange.Value);
+		}
+
+		/// <summary>
+		/// Makes sure weapons with blind spots have sane values for their minimum/maximum range.
+		/// </summary>
+		[TestMethod]
+		public void WeaponRangeBlindSpot()
+		{
+			Mod.Load("ComponentLoaderTest");
+			var w = Mod.Current.ComponentTemplates.FindByName("Blind Spot Weapon");
+			Assert.AreEqual(2, w.WeaponInfo.MinRange.Value);
+			Assert.AreEqual(3, w.WeaponInfo.MaxRange.Value);
+		}
+	}
+}
--- FrEee/Modding/Loaders/ComponentLoader.cs
+++ FrEee/Modding/Loaders/ComponentLoader.cs
@@ -12,235 +12,225 @@ using System.Linq;
 
 namespace FrEee.Modding.Loaders
 {
-    /// <summary>
-    /// Loads components from Components.txt.
-    /// </summary>
-    public class ComponentLoader : DataFileLoader
-    {
-        #region Public Fields
+	/// <summary>
+	/// Loads components from Components.txt.
+	/// </summary>
+	public class ComponentLoader : DataFileLoader
+	{
+		#region Public Fields
 
-        public const string Filename = "Components.txt";
+		public const string Filename = "Components.txt";
 
-        #endregion Public Fields
+		#endregion Public Fields
 
-        #region Private Fields
+		#region Private Fields
 
-        private static List<string> numbers = new List<string>()
-        {
-            "Zero", "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten"
-        };
+		private static List<string> numbers = new List<string>()
+		{
+			"Zero", "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten"
+		};
 
-        #endregion Private Fields
+		#endregion Private Fields
 
-        #region Public Constructors
+		#region Public Constructors
 
-        public ComponentLoader(string modPath)
-                    : base(modPath, Filename, DataFile.Load(modPath, Filename))
-        {
-        }
+		public ComponentLoader(string modPath)
+					: base(modPath, Filename, DataFile.Load(modPath, Filename))
+		{
+		}
 
-        #endregion Public Constructors
+		#endregion Public Constructors
 
-        #region Public Methods
+		#region Public Methods
 
-        public override IEnumerable<IModObject> Load(Mod mod)
-        {
-            foreach (var rec in DataFile.Records)
-            {
-                var c = new ComponentTemplate();
-                mod.ComponentTemplates.Add(c);
+		public override IEnumerable<IModObject> Load(Mod mod)
+		{
+			foreach (var rec in DataFile.Records)
+			{
+				var c = new ComponentTemplate();
+				mod.ComponentTemplates.Add(c);
 
-                int index = -1;
+				int index = -1;
 
-                c.ModID = rec.Get<string>("ID", c);
-                c.Name = rec.Get<string>("Name", c);
-                c.Description = rec.Get<string>("Description", c);
+				c.ModID = rec.Get<string>("ID", c);
+				c.Name = rec.Get<string>("Name", c);
+				c.Description = rec.Get<string>("Description", c);
 
-                var picfield = rec.FindField("Pic", ref index, false, 0, true);
-                if (picfield != null)
-                    c.PictureName = picfield.CreateFormula<string>(c);
-                else
-                    c.PictureName = "Comp_" + rec.Get<int>("Pic Num", c).Value.ToString("000"); // for compatibility with SE4
+				var picfield = rec.FindField("Pic", ref index, false, 0, true);
+				if (picfield != null)
+					c.PictureName = picfield.CreateFormula<string>(c);
+				else
+					c.PictureName = "Comp_" + rec.Get<int>("Pic Num", c).Value.ToString("000"); // for compatibility with SE4
 
-                c.Size = rec.Get<int>("Tonnage Space Taken", c);
-                c.Durability = rec.Get<int>("Tonnage Structure", c);
+				c.Size = rec.Get<int>("Tonnage Space Taken", c);
+				c.Durability = rec.Get<int>("Tonnage Structure", c);
 
-                foreach (var costfield in rec.Fields.Where(cf => cf.Name.StartsWith("Cost ")))
-                    c.Cost[Resource.Find(costfield.Name.Substring("Cost ".Length))] = costfield.CreateFormula<int>(c);
+				foreach (var costfield in rec.Fields.Where(cf => cf.Name.StartsWith("Cost ")))
+					c.Cost[Resource.Find(costfield.Name.Substring("Cost ".Length))] = costfield.CreateFormula<int>(c);
 
-                var vtoverridefield = rec.FindField(new string[] { "Vehicle List Type Override", "Vechicle List Type Override" }, ref index, false, 0, true); // silly Aaron can't spell "vehicle"
-                if (vtoverridefield != null)
-                    c.VehicleTypes = ParseVehicleTypes(vtoverridefield.Value, ",", rec);
-                else
-                    c.VehicleTypes = ParseVehicleTypes(rec.Get<string>("Vehicle Type", c), @"\", rec);
+				var vtoverridefield = rec.FindField(new string[] { "Vehicle List Type Override", "Vechicle List Type Override" }, ref index, false, 0, true); // silly Aaron can't spell "vehicle"
+				if (vtoverridefield != null)
+					c.VehicleTypes = ParseVehicleTypes(vtoverridefield.Value, ",", rec);
+				else
+					c.VehicleTypes = ParseVehicleTypes(rec.Get<string>("Vehicle Type", c), @"\", rec);
 
-                c.SupplyUsage = rec.Get<int>("Supply Amount Used", c);
+				c.SupplyUsage = rec.Get<int>("Supply Amount Used", c);
 
-                var restrictions = rec.Get<string>("Restrictions", c);
-                if (!string.IsNullOrEmpty(restrictions) && restrictions != "None")
-                {
-                    var word = restrictions.Value.Split(' ').First();
-                    int num;
-                    if (numbers.Contains(word))
-                        c.MaxPerVehicle = numbers.IndexOf(word);
-                    else if (int.TryParse(word, out num))
-                        c.MaxPerVehicle = num;
-                    else
-                        Mod.Errors.Add(new DataParsingException("Can't parse \"" + word + "\" as a max-per-vehicle restriction.", Mod.CurrentFileName, rec));
-                }
+				var restrictions = rec.Get<string>("Restrictions", c);
+				if (!string.IsNullOrEmpty(restrictions) && restrictions != "None")
+				{
+					var word = restrictions.Value.Split(' ').First();
+					int num;
+					if (numbers.Contains(word))
+						c.MaxPerVehicle = numbers.IndexOf(word);
+					else if (int.TryParse(word, out num))
+						c.MaxPerVehicle = num;
+					else
+						Mod.Errors.Add(new DataParsingException("Can't parse \"" + word + "\" as a max-per-vehicle restriction.", Mod.CurrentFileName, rec));
+				}
 
-                c.Group = rec.Get<string>("General Group", c);
-                c.Family = rec.Get<string>("Family", c);
-                c.RomanNumeral = rec.Get<int>("Roman Numeral", c);
-                c.StellarConstructionGroup = rec.Get<string>("Custom Group", c);
+				c.Group = rec.Get<string>("General Group", c);
+				c.Family = rec.Get<string>("Family", c);
+				c.RomanNumeral = rec.Get<int>("Roman Numeral", c);
+				c.StellarConstructionGroup = rec.Get<string>("Custom Group", c);
 
-                foreach (var tr in RequirementLoader.LoadEmpireRequirements(rec, c, RequirementType.Unlock))
-                    c.UnlockRequirements.Add(tr);
+				foreach (var tr in RequirementLoader.LoadEmpireRequirements(rec, c, RequirementType.Unlock))
+					c.UnlockRequirements.Add(tr);
 
-                // TODO - build and use requirements
+				// TODO - build and use requirements
 
-                foreach (var abil in AbilityLoader.Load(Filename, rec, c))
-                    c.Abilities.Add(abil);
+				foreach (var abil in AbilityLoader.Load(Filename, rec, c))
+					c.Abilities.Add(abil);
 
-                var wfield = rec.FindField("Weapon Type", ref index, false, 0, true);
-                if (wfield != null)
-                {
-                    WeaponInfo w = null;
-                    if (wfield.Value == "Seeking" || wfield.Value == "Seeking Point-Defense")
-                    {
-                        var sw = new SeekingWeaponInfo();
-                        sw.SeekerSpeed = rec.Get<int>("Weapon Seeker Speed", c);
-                        sw.SeekerDurability = rec.Get<int>("Weapon Seeker Dmg Res", c);
-                        w = sw;
-                    }
-                    else if (wfield.Value == "Direct Fire" || wfield.Value == "Point-Defense")
-                    {
-                        var dfw = new DirectFireWeaponInfo();
-                        dfw.AccuracyModifier = rec.Get<int>("Weapon Modifier", c);
-                        w = dfw;
-                    }
-                    else if (wfield.Value == "Warhead" || wfield.Value == "Warhead Point-Defense")
-                    {
-                        var ww = new WarheadWeaponInfo();
-                        w = ww;
-                    }
-                    else if (string.IsNullOrEmpty(wfield.Value) || wfield.Value == "None")
-                        w = null;
-                    else
-                        Mod.Errors.Add(new DataParsingException("Invalid weapon type \"" + wfield.Value + "\".", Mod.CurrentFileName, rec, wfield));
-                    if (w != null)
-                    {
-                        if (wfield.Value.EndsWith("Point-Defense"))
-                            w.IsPointDefense = true;
+				var wfield = rec.FindField("Weapon Type", ref index, false, 0, true);
+				if (wfield != null)
+				{
+					WeaponInfo w = null;
+					if (wfield.Value == "Seeking" || wfield.Value == "Seeking Point-Defense")
+					{
+						var sw = new SeekingWeaponInfo();
+						sw.SeekerSpeed = rec.Get<int>("Weapon Seeker Speed", c);
+						sw.SeekerDurability = rec.Get<int>("Weapon Seeker Dmg Res", c);
+						w = sw;
+					}
+					else if (wfield.Value == "Direct Fire" || wfield.Value == "Point-Defense")
+					{
+						var dfw = new DirectFireWeaponInfo();
+						dfw.AccuracyModifier = rec.Get<int>("Weapon Modifier", c);
+						w = dfw;
+					}
+					else if (wfield.Value == "Warhead" || wfield.Value == "Warhead Point-Defense")
+					{
+						var ww = new WarheadWeaponInfo();
+						w = ww;
+					}
+					else if (string.IsNullOrEmpty(wfield.Value) || wfield.Value == "None")
+						w = null;
+					else
+						Mod.Errors.Add(new DataParsingException("Invalid weapon type \"" + wfield.Value + "\".", Mod.CurrentFileName, rec, wfield));
+					if (w != null)
+					{
+						if (wfield.Value.EndsWith("Point-Defense"))
+							w.IsPointDefense = true;
 
-                        var wtoverridefield = rec.FindField("Weapon List Target Override", ref index, false, 0, true);
-                        if (wtoverridefield != null)
-                            w.Targets = ParseWeaponTargets(wtoverridefield.Value, ",", rec);
-                        else
-                            w.Targets = ParseWeaponTargets(rec.Get<string>("Weapon Target", c), @"\", rec);
+						var wtoverridefield = rec.FindField("Weapon List Target Override", ref index, false, 0, true);
+						if (wtoverridefield != null)
+							w.Targets = ParseWeaponTargets(wtoverridefield.Value, ",", rec);
+						else
+							w.Targets = ParseWeaponTargets(rec.Get<string>("Weapon Target", c), @"\", rec);
 
-                        w.MinRange = rec.Get<int>(new string[] { "Min Range", "Minimum Range", "Weapon Min Range", "Weapon Minimum Range" }, c) ?? 0;
-                        w.MaxRange = rec.Get<int>(new string[] { "Max Range", "Maximum Range", "Weapon Max Range", "Weapon Maximum Range" }, c) ?? 20;
-                        var dmgfield = rec.FindField(new string[] { "Damage", "Weapon Damage", "Damage At Rng", "Weapon Damage At Rng", "Damage At Range", "Weapon Damage At Range" }, ref index);
-                        if (dmgfield.Value.StartsWith("="))
-                            w.Damage = dmgfield.CreateFormula<int>(c);
-                        else
-                        {
-                            string dmgstr = null;
-                            try
-                            {
-                                dmgstr = dmgfield.Value;
-                                var dmg = dmgstr.Split(' ').Select(s => int.Parse(s)).ToList();
-                                int firstNonzero = 0;
-                                while (dmg.Count > 0 && firstNonzero < dmg.Count && dmg[firstNonzero] == 0)
-                                    firstNonzero++;
-                                int lastNonzero = w.MaxRange - w.MinRange;
-                                while (dmg.Count > lastNonzero && dmg[lastNonzero] == 0)
-                                    lastNonzero--;
-                                w.MaxRange = w.MinRange + lastNonzero;
-                                var dict = new Dictionary<int, int>();
-                                for (int i = firstNonzero; i <= lastNonzero - firstNonzero; i++)
-                                {
-                                    if (dmg.Count > i)
-                                        dict.Add(i + w.MinRange.Value, dmg[i]);
-                                    else
-                                        w.MaxRange = (int)w.MaxRange - 1;
-                                }
-                                if (firstNonzero > lastNonzero)
-                                    w.Damage = 0;
-                                else
-                                {
-                                    var idx = -1;
-                                    if (w.MinRange == 1 && rec.FindField(new string[] { "Min Range", "Minimum Range" }, ref idx) == null)
-                                    {
-                                        // used default min range of one; assign a damage value at range zero which is equal to value at range one
-                                        w.MinRange = 0;
-                                        dict.Add(0, dmg[0]);
-                                    }
-                                    w.Damage = dict.BuildMultiConditionalLessThanOrEqual(c, "range", 0);
-                                }
-                            }
-                            catch (Exception ex)
-                            {
-                                Mod.Errors.Add(new DataParsingException("Can't parse \"" + dmgstr + "\" as a damage string: " + ex.Message, Mod.CurrentFileName, rec));
-                            }
-                        }
+						w.MinRange = rec.Get<int>(new string[] { "Min Range", "Minimum Range", "Weapon Min Range", "Weapon Minimum Range" }, c) ?? 0;
+						w.MaxRange = rec.Get<int>(new string[] { "Max Range", "Maximum Range", "Weapon Max Range", "Weapon Maximum Range" }, c) ?? 20;
+						var dmgfield = rec.FindField(new string[] { "Damage", "Weapon Damage", "Damage At Rng", "Weapon Damage At Rng", "Damage At Range", "Weapon Damage At Range" }, ref index);
+						if (dmgfield.Value.StartsWith("="))
+							w.Damage = dmgfield.CreateFormula<int>(c);
+						else
+						{
+							string dmgstr = null;
+							try
+							{
+								var dict = new SafeDictionary<int, int>();
+								var split = dmgfield.Value.Split(' ');
+								for (var i = 0; i < split.Length; i++)
+								{
+									if (split[i].ToInt() == 0)
+										continue;
+									dict[i + 1] = split[i].ToInt();
+								}
 
-                        var damTypeName = rec.Get<string>("Weapon Damage Type", c);
-                        w.DamageType = Mod.Current.DamageTypes.FindByName(damTypeName);
+								// HACK - SE4 doesn't explicitly specify damage at range zero so copy the damage at range one value
+								if (dict[1] != 0)
+								{
+									dict[0] = dict[1];
+									w.MinRange = 0;
+								}
 
-                        if (w.DamageType == null)
-                        {
-                            // no valid damage type? then make it normal damage and log a warning
-                            w.DamageType = DamageType.Normal;
-                            Mod.Errors.Add(new DataParsingException("Unknown damage type \"" + damTypeName + "\"; setting " + c + "'s damage type to Normal.", Mod.CurrentFileName, rec));
-                        }
+								w.MinRange = dict.Keys.Min();
+								w.MaxRange = dict.Keys.Max();
 
-                        w.ReloadRate = rec.Get<double>("Weapon Reload Rate", c);
+								w.Damage = dict.BuildMultiConditionalLessThanOrEqual(c, "range", 0);
+							}
+							catch (Exception ex)
+							{
+								Mod.Errors.Add(new DataParsingException("Can't parse \"" + dmgstr + "\" as a damage string: " + ex.Message, Mod.CurrentFileName, rec));
+							}
+						}
 
-                        var wdisptype = rec.Get<string>("Weapon Display Type", c);
-                        var wdispname = rec.Get<string>("Weapon Display", c);
-                        if (wdisptype == "Beam")
-                            w.DisplayEffect = new BeamWeaponDisplayEffect(wdispname);
-                        else if (wdisptype == "Torp" || wdisptype == "Torpedo" || wdisptype == "Projectile")
-                            w.DisplayEffect = new ProjectileWeaponDisplayEffect(wdispname);
-                        else if (wdisptype == "Seeker")
-                            w.DisplayEffect = new SeekerWeaponDisplayEffect(wdispname);
-                        else
-                            Mod.Errors.Add(new DataParsingException("Invalid weapon display effect type \"" + wdisptype + "\".", Mod.CurrentFileName, rec));
+						var damTypeName = rec.Get<string>("Weapon Damage Type", c);
+						w.DamageType = Mod.Current.DamageTypes.FindByName(damTypeName);
 
-                        // sanity check
-                        if (wdisptype == "Beam" && w is SeekingWeaponInfo)
-                            Mod.Errors.Add(new DataParsingException("A seeking weapon cannot use a beam display effect.", Mod.CurrentFileName, rec));
+						if (w.DamageType == null)
+						{
+							// no valid damage type? then make it normal damage and log a warning
+							w.DamageType = DamageType.Normal;
+							Mod.Errors.Add(new DataParsingException("Unknown damage type \"" + damTypeName + "\"; setting " + c + "'s damage type to Normal.", Mod.CurrentFileName, rec));
+						}
 
-                        w.Sound = rec.Get<string>("Weapon Sound", c);
-                        w.Family = rec.Get<string>("Weapon Family", c);
-                    }
-                    c.WeaponInfo = w;
+						w.ReloadRate = rec.Get<double>("Weapon Reload Rate", c);
 
-                    yield return c;
-                }
-            }
-        }
+						var wdisptype = rec.Get<string>("Weapon Display Type", c);
+						var wdispname = rec.Get<string>("Weapon Display", c);
+						if (wdisptype == "Beam")
+							w.DisplayEffect = new BeamWeaponDisplayEffect(wdispname);
+						else if (wdisptype == "Torp" || wdisptype == "Torpedo" || wdisptype == "Projectile")
+							w.DisplayEffect = new ProjectileWeaponDisplayEffect(wdispname);
+						else if (wdisptype == "Seeker")
+							w.DisplayEffect = new SeekerWeaponDisplayEffect(wdispname);
+						else
+							Mod.Errors.Add(new DataParsingException("Invalid weapon display effect type \"" + wdisptype + "\".", Mod.CurrentFileName, rec));
 
-        #endregion Public Methods
+						// sanity check
+						if (wdisptype == "Beam" && w is SeekingWeaponInfo)
+							Mod.Errors.Add(new DataParsingException("A seeking weapon cannot use a beam display effect.", Mod.CurrentFileName, rec));
 
-        #region Private Methods
+						w.Sound = rec.Get<string>("Weapon Sound", c);
+						w.Family = rec.Get<string>("Weapon Family", c);
+					}
+					c.WeaponInfo = w;
 
-        private VehicleTypes ParseVehicleTypes(string s, string separator, Record rec)
-        {
-            if (s == null)
-                return VehicleTypes.All;
-            return s.ParseEnum<VehicleTypes>();
-        }
+					yield return c;
+				}
+			}
+		}
 
-        private WeaponTargets ParseWeaponTargets(string s, string separator, Record rec)
-        {
-            if (s == null)
-                return WeaponTargets.All;
-            return s.ParseEnum<WeaponTargets>();
-        }
+		#endregion Public Methods
 
-        #endregion Private Methods
-    }
+		#region Private Methods
+
+		private VehicleTypes ParseVehicleTypes(string s, string separator, Record rec)
+		{
+			if (s == null)
+				return VehicleTypes.All;
+			return s.ParseEnum<VehicleTypes>();
+		}
+
+		private WeaponTargets ParseWeaponTargets(string s, string separator, Record rec)
+		{
+			if (s == null)
+				return WeaponTargets.All;
+			return s.ParseEnum<WeaponTargets>();
+		}
+
+		#endregion Private Methods
+	}
 }
