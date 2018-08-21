e7b2b0e64978b16609283bc51c431b0375a78d10
ekolis <ekolis@localhost>
2018-08-21 13:41:28 -0400

    Fixed bug in PRNG that was causing long values to be out of range.

---
+++ FrEee.Tests/Utility/PrngTest.cs
@@ -0,0 +1,32 @@
+﻿using FrEee.Utility;
+using FrEee.Utility.Extensions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace FrEee.Tests.Utility
+{
+	/// <summary>
+	/// Tries to test the random number generator. As much as a RNG is testable...
+	/// </summary>
+	[TestClass]
+	public class PrngTest
+	{
+		private PRNG prng;
+
+		[TestInitialize]
+		public void Init()
+		{
+			prng = new PRNG(42);
+		}
+
+		[TestMethod]
+		public void NextLong()
+		{
+			for (var i = 0; i < 1000; i++)
+			{
+				long max = 1234567890;
+				Assert.IsFalse(prng.Next(max) > max);
+			}
+		}
+	}
+}
