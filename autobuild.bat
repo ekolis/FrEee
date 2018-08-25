ec74383f88b5b912cad63e90f160fcc6f90e52d1
ekolis <ekolis@localhost>
2018-08-25 10:54:08 -0400

    Updated autobuild script for VS2017

--- autobuild.bat
+++ autobuild.bat
@@ -1,6 +1,7 @@
-REM this script will build FrEee, assuming you have the .NET Framework 4.6 and MSBuild installed
-REM .NET 4.6: https://www.microsoft.com/en-us/download/details.aspx?id=48130
-REM MSBuild: https://go.microsoft.com/fwlink/?LinkId=615458
+@echo off
+REM this script will build FrEee, assuming you have the .NET Framework 4.7.2 and MSBuild installed
+REM .NET 4.7.2: https://www.microsoft.com/net/download/thank-you/net472
+REM MSBuild: https://visualstudio.microsoft.com/thank-you-downloading-visual-studio/?sku=BuildTools&rel=15#
 REM
 REM if you create the following lines (without REM) in your .hg/hgrc file then it will automatically run when you update (handy when pulling other developers' changes)
 REM
@@ -13,4 +14,10 @@ REM before running this script you might first want to run clone-dependencies.ba
 REM using those scripts will require you have Mercurial installed
 REM my favorite Mercurial client: http://tortoisehg.bitbucket.org/
 
-if exist "C:\Program Files\MSBuild\14.0\Bin\msbuild.exe" ("C:\Program Files\MSBuild\14.0\Bin\msbuild.exe" FrEee.sln) else if exist "C:\Program Files (x86)\MSBuild\14.0\Bin\msbuild.exe" ("C:\Program Files (x86)\MSBuild\14.0\Bin\msbuild.exe" FrEee.sln) else echo MSBuild is not installed! Download it from https://go.microsoft.com/fwlink/?LinkId=615458
+if exist "C:\Program Files\Microsoft Visual Studio\2017\BuildTools\MSBuild\15.0\Bin\msbuild.exe" (
+	("C:\Program Files\Microsoft Visual Studio\2017\BuildTools\MSBuild\15.0\Bin\msbuild.exe" FrEee.sln)
+) else if exist "C:\Program Files (x86)\Microsoft Visual Studio\2017\BuildTools\MSBuild\15.0\Bin\msbuild.exe" (
+	("C:\Program Files (x86)\Microsoft Visual Studio\2017\BuildTools\MSBuild\15.0\Bin\msbuild.exe" FrEee.sln)
+) else (
+	echo MSBuild v15 is not installed! Download it from https://visualstudio.microsoft.com/thank-you-downloading-visual-studio/?sku=BuildTools^&rel=15#
+)
