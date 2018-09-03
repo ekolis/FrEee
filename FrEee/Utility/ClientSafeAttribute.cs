402faa0fbc8830bc820b96b750fa6166db2118a7
Ed Kolis <edkolis@gmail.com>
2018-09-03 13:14:16 -0400

    Really fixed PlayerInfo client safety.

--- FrEee/Utility/ClientSafeAttribute.cs
+++ FrEee/Utility/ClientSafeAttribute.cs
@@ -6,6 +6,7 @@ namespace FrEee.Utility
 	/// Marks a type as safe to pass from the client to the server.
 	/// </summary>
 	[AttributeUsage(AttributeTargets.Class | AttributeTargets.Struct | AttributeTargets.Interface, Inherited = true, AllowMultiple = false)]
+	[Obsolete("ClientSafeAttribute is obsolete; please implement IPromotable instead.")]
 	internal sealed class ClientSafeAttribute : Attribute
 	{
 	}
