ed792d396087c62d6ccb1c1f0a41ed5bf6c874d2
Ed Kolis <edkolis@gmail.com>
2018-09-12 17:07:56 -0400

    Added some more stringifiers.

---
+++ FrEee/Utility/Stringifiers/IntLiteralFormulaStringifier.cs
@@ -0,0 +1,25 @@
+﻿using FrEee.Modding;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.Composition;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FrEee.Utility.Stringifiers
+{
+	[Export(typeof(IStringifier))]
+	public class IntLiteralFormulaStringifier : Stringifier<LiteralFormula<int>>
+	{
+		public override LiteralFormula<int> Destringify(string s)
+		{
+			return new LiteralFormula<int>(s);
+		}
+
+		public override string Stringify(LiteralFormula<int> t)
+		{
+			return t.Text;
+		}
+	}
+}
