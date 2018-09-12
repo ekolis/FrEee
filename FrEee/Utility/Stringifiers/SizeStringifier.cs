87adf7d06533303be53fc608a4f7cc0cdf760aa8
Ed Kolis <edkolis@gmail.com>
2018-09-12 16:45:54 -0400

    Added a few basic stringifiers.

---
+++ FrEee/Utility/Stringifiers/SizeStringifier.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Composition;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FrEee.Utility.Stringifiers
+{
+	[Export(typeof(IStringifier))]
+	public class PointStringifier : Stringifier<Point>
+	{
+		public override Point Destringify(string s)
+		{
+			var split = s.Split(',').Select(x => x.Trim()).ToArray();
+			return new Point(int.Parse(split[0]), int.Parse(split[1]));
+		}
+
+		public override string Stringify(Point t)
+		{
+			return $"{t.X}, {t.Y}";
+		}
+	}
+}
