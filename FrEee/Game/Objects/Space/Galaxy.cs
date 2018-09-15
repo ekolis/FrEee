cbae1923ef8d0adc77d2e7de816693530b3239ca
Ed Kolis <edkolis@gmail.com>
2018-09-14 21:49:22 -0400

    Don't trigger a space battle if no one has any weapons.

--- FrEee/Game/Objects/Space/Galaxy.cs
+++ FrEee/Game/Objects/Space/Galaxy.cs
@@ -1665,7 +1665,7 @@ namespace FrEee.Game.Objects.Space
 				var sector = v.Sector;
 				if (v.Owner != null && // unowned objects can't pick fights
 					sector != null && // can't fight nowhere
-					sector.SpaceObjects.OfType<ICombatant>().Any(sobj => sobj.IsHostileTo(v.Owner) || v.IsHostileTo(sobj.Owner)) && // any enemies?
+					sector.SpaceObjects.OfType<ICombatant>().Any(sobj => sobj.IsHostileTo(v.Owner) && sobj.Weapons.Any() || v.IsHostileTo(sobj.Owner) && v.Weapons.Any()) && // any enemies?
 					(!lastBattleTimestamps.ContainsKey(sector) || lastBattleTimestamps[sector] < Timestamp - (v.StrategicSpeed == 0 ? 1d : 1d / v.StrategicSpeed))) // have we fought here too recently?
 				{
 					// resolve the battle
