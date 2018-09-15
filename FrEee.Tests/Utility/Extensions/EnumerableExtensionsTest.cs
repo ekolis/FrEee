ec9047264789ec362b908e834c4126963fd9f8f3
Ed Kolis <edkolis@gmail.com>
2018-09-15 13:19:55 -0400

    Hopefully fixed empty fleet bug

---
+++ FrEee.Tests/Utility/Extensions/EnumerableExtensionsTest.cs
@@ -0,0 +1,21 @@
+﻿using FrEee.Utility.Extensions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace FrEee.Tests.Utility.Extensions
+{
+	/// <summary>
+	/// Tests extension methods.
+	/// </summary>
+	[TestClass]
+	public class EnumerableExtensionsTest
+	{
+		[TestMethod]
+		public void ExceptSingle()
+		{
+			var list = new object[] { 42, "fred", null };
+			Assert.AreEqual(2, list.ExceptSingle(null).Count());
+		}
+	}
+}
