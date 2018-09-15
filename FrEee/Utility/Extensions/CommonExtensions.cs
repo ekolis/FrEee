8c40d0a25a7216e789dc72761dd48437718916db
Ed Kolis <edkolis@gmail.com>
2018-09-14 22:45:15 -0400

    Need to send "hit a minefield" message to an actual player, a destroyed flet has no owner...

--- FrEee/Utility/Extensions/CommonExtensions.cs
+++ FrEee/Utility/Extensions/CommonExtensions.cs
@@ -173,6 +173,7 @@ namespace FrEee.Utility.Extensions
 		{
 			if (sobj is IDamageable && sobj is IOwnable)
 			{
+				var owner = sobj.Owner;
 				var d = (IDamageable)sobj;
 				var sector = sobj.Sector;
 				if (sector == null)
@@ -230,7 +231,7 @@ namespace FrEee.Utility.Extensions
 
 				// logging!
 				if (minesDetonated.Any() || minesSwept.Any() || minesAttacking.Any())
-					sobj.Owner.Log.Add(sobj.CreateLogMessage(sobj + " encountered a mine field at " + sector + " and took " + totalDamage + " points of damage, sweeping " + minesSwept.Sum(kvp => kvp.Value) + " mines."));
+					owner.Owner.Log.Add(sobj.CreateLogMessage(sobj + " encountered a mine field at " + sector + " and took " + totalDamage + " points of damage, sweeping " + minesSwept.Sum(kvp => kvp.Value) + " mines."));
 				foreach (var emp in minesSwept.Keys.Union(minesDetonated.Keys).Union(minesAttacking.Keys))
 					emp.Log.Add(sobj.CreateLogMessage(sobj + " encountered our mine field at " + sector + ". " + minesDetonated[emp] + " of our mines detonated, " + minesAttacking[emp] + " others fired weapons, and " + minesSwept[emp] + " were swept. " + sector.SpaceObjects.OfType<Mine>().Where(m => m.Owner == emp).Count() + " mines remain in the sector."));
 			}
