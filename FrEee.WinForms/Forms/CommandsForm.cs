039d294fb7139b469000c32bc6d9730b28232e43
Ed Kolis <edkolis@gmail.com>
2019-02-24 00:15:32 -0500

    Added a command list view but it's not rendering for some reason

---
+++ FrEee.WinForms/Forms/CommandsForm.cs
@@ -0,0 +1,35 @@
+﻿using FrEee.Game.Interfaces;
+using FrEee.Game.Objects.Civilization;
+using FrEee.Game.Objects.Combat;
+using FrEee.Game.Objects.LogMessages;
+using FrEee.Game.Objects.Space;
+using FrEee.Game.Objects.Technology;
+using FrEee.Modding.Templates;
+using FrEee.Utility.Extensions;
+using FrEee.WinForms.Utility.Extensions;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace FrEee.WinForms.Forms
+{
+	public partial class CommandsForm : GameForm
+	{ 
+		public CommandsForm()
+		{
+			InitializeComponent();
+
+			try { this.Icon = new Icon(FrEee.WinForms.Properties.Resources.FrEeeIcon); } catch { }
+		}
+
+		private void CommandsForm_Load(object sender, EventArgs e)
+		{
+			lstCommands.Initialize(32, 32);
+			foreach (var cmd in Empire.Current.Commands)
+				lstCommands.AddItemWithImage(null, cmd.ToString(), cmd, (cmd.Executor is IPictorial p ? p.Icon : null));
+		}
+	}
+}
