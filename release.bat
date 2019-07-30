ef3b7088305891bade8fc004d375354b99b8f8bc
Ed Kolis <edkolis@gmail.com>
2019-07-30 17:56:59 -0400

    Updated release.bat to use msbuild v16 (VS2019).

--- release.bat
+++ release.bat
@@ -1,9 +1,9 @@
-if exist "C:\Program Files\Microsoft Visual Studio\2017\BuildTools\MSBuild\15.0\Bin\msbuild.exe" (
-	("C:\Program Files\Microsoft Visual Studio\2017\BuildTools\MSBuild\15.0\Bin\msbuild.exe" FrEee.sln /p:Configuration=Release)
-) else if exist "C:\Program Files (x86)\Microsoft Visual Studio\2017\BuildTools\MSBuild\15.0\Bin\msbuild.exe" (
-	("C:\Program Files (x86)\Microsoft Visual Studio\2017\BuildTools\MSBuild\15.0\Bin\msbuild.exe" FrEee.sln /p:Configuration=Release)
+if exist "C:\Program Files\Microsoft Visual Studio\2019\BuildTools\MSBuild\Current\Bin\msbuild.exe" (
+	("C:\Program Files\Microsoft Visual Studio\2019\BuildTools\MSBuild\Current\Bin\msbuild.exe" FrEee.sln /p:Configuration=Release)
+) else if exist "C:\Program Files (x86)\Microsoft Visual Studio\2019\BuildTools\MSBuild\Current\Bin\msbuild.exe" (
+	("C:\Program Files (x86)\Microsoft Visual Studio\2019\BuildTools\MSBuild\Current\Bin\msbuild.exe" FrEee.sln /p:Configuration=Release)
 ) else (
-	echo MSBuild v15 is not installed! Download it from https://visualstudio.microsoft.com/thank-you-downloading-visual-studio/?sku=BuildTools^&rel=15#
+	echo MSBuild v16 is not installed! Download it from https://visualstudio.microsoft.com/thank-you-downloading-visual-studio/?sku=BuildTools&rel=16 please.
 )
 cd FrEee.WinForms\bin
 ren Release FrEee
