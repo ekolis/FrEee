a9d8c6e5f5dd835ef8a2a805acb8bb6954f8293b
KarbonKitty <308lapua.magnum@gmail.com>
2020-04-28 19:53:40 +0200

    Fix duplicate GUID and post build message in batch file

--- FrEee.WinForms/post-build.bat
+++ FrEee.WinForms/post-build.bat
@@ -2,7 +2,7 @@
 REM concatenate all arguments except the first with spaces between them
 for /f "tokens=1,* delims= " %%a in ("%*") do set ALL_BUT_FIRST=%%b
 
-echo Copying assets to %ALL_BUT_FIRST%\bin\%1\net472
+echo Copying assets to %ALL_BUT_FIRST%\%1
 
 robocopy "%ALL_BUT_FIRST%\..\FrEee\Mods" "%ALL_BUT_FIRST%\%1\Mods" /e
 robocopy "%ALL_BUT_FIRST%\..\FrEee\Data" "%ALL_BUT_FIRST%\%1\Data" /e
