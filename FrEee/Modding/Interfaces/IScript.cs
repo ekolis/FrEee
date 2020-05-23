2df42864908f79d676e54b4ed45d48dcd50686cd
KarbonKitty <308lapua.magnum@gmail.com>
2020-05-23 19:09:50 +0200

    Removed commented out code to fix warning

--- FrEee/Modding/Interfaces/IScript.cs
+++ FrEee/Modding/Interfaces/IScript.cs
@@ -1,16 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
 namespace FrEee.Modding
 {
 	/// <summary>
-	/// A type of script. 
+	/// A type of script.
 	/// </summary>
-    public interface IScript
-    {
+	public interface IScript
+	{
 		/// <summary>
 		/// The name of this script module. This should be a valid Python module name.
 		/// </summary>
@@ -20,11 +14,5 @@ namespace FrEee.Modding
 		/// The script text.
 		/// </summary>
 		string Text { get; set; }
-
-
-		/// <summary>
-		/// Any external scripts directly referenced by this script.
-		/// </summary>
-		//ISet<IScript> ExternalScripts { get; set; }
 	}
 }
