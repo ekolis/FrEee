c1efd9e479f575eda919347178a39c71d4d5a746
Ed Kolis <edkolis@gmail.com>
2020-05-23 00:59:05 -0400

    Update copyright date etc (#107)

--- FrEee/Properties/AssemblyInfo.cs
+++ FrEee/Properties/AssemblyInfo.cs
@@ -9,7 +9,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("FrEee")]
-[assembly: AssemblyCopyright("Copyright ©  2013-2018")]
+[assembly: AssemblyCopyright("Copyright © 2013-2020")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
