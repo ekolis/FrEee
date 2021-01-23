7561b1e03ae25c416aa56dd639233c1e2d4e2311
Ed Kolis <edkolis@gmail.com>
2021-01-23 18:58:00 -0500

    Oh, we did need that post-build script after all. But we don't need the asset copy commands.

--- FrEee.WinForms/post-build.sh
+++ FrEee.WinForms/post-build.sh
@@ -4,13 +4,5 @@
 
 # probably outdated!
 
-cp -a -u -v ../FrEee/Mods bin/Debug/Mods
-cp -a -u -v ../FrEee/Data bin/Debug/Data
-cp -a -u -v ../FrEee/GameSetups bin/Debug/GameSetups
-cp -a -u -v ../FrEee/Docs bin/Debug/Docs
-cp -a -u -v ../FrEee/Music bin/Debug/Music
-cp -a -u -v ../FrEee/Pictures bin/Debug/Pictures
-cp -a -u -v ../FrEee/Licenses bin/Debug/Licenses
-cp -a -u -v ../FrEee/Scripts bin/Debug/Scripts
-cp -a -u -v ../FrEee/Dsgnname bin/Debug/Dsgnname
+# this file is obsolete, there is no need to script copying of assets anymore
 
