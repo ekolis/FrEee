3ebe3cdbf71cbcafddedaff5807721b12a7b713b
Ed Kolis <edkolis@gmail.com>
2021-01-23 04:19:43 -0500

    Let's make a blank Avalonia project...

---
+++ FrEee.Avalonia/ViewModels/ViewModelBase.cs
@@ -0,0 +1,11 @@
+using ReactiveUI;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FrEee.Avalonia.ViewModels
+{
+	public class ViewModelBase : ReactiveObject
+	{
+	}
+}
