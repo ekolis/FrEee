341df222db535e0d3e59ef74474fbfc3a4be4f5f
Ed Kolis <edkolis@gmail.com>
2024-01-13 15:29:27 -0500

    Move namespaces in tests

---
+++ FrEee.Tests/Objects/Orders/ConstructionTest.cs
@@ -0,0 +1,66 @@
+﻿using FrEee.Objects.Civilization;
+using FrEee.Objects.Space;
+using FrEee.Objects.Technology;
+using FrEee.Modding;
+using FrEee.Extensions;
+using NUnit.Framework;
+
+namespace FrEee.Tests.Objects.Orders
+{
+	/// <summary>
+	/// Tests construction queue capabilities.
+	/// </summary>
+	public class ConstructionTest
+	{
+		private static Colony colony;
+		private static Empire empire;
+		private static Planet planet;
+		private static Race race;
+		private static FacilityTemplate sy;
+
+		[OneTimeSetUp]
+		public static void ClassInit()
+		{
+			// initialize galaxy
+			new Galaxy();
+			Mod.Load(null);
+
+			// initialize empires
+			empire = new Empire();
+			empire.Name = "Engi";
+			race = new Race();
+			race.Name = "Engi";
+			race.Aptitudes["Construction"] = 100;
+
+			// initialize components
+			sy = Mod.Current.FacilityTemplates.FindByName("Space Yard Facility I");
+		}
+
+		[SetUp]
+		public void Init()
+		{
+			// initialize colony
+			planet = new Planet();
+			colony = new Colony();
+			colony.Facilities.Add(new Facility(sy));
+			colony.Population.Add(race, (long)1e9); // 1 billion population;
+			colony.ConstructionQueue = new ConstructionQueue(planet);
+			planet.Colony = colony;
+		}
+
+		/// <summary>
+		/// If there's no population on a colony, it shouldn't be able to build anything.
+		/// </summary>
+		[Test]
+		public void NoPopNoBuild()
+		{
+			colony.Population.Clear();
+			Assert.IsTrue(planet.ConstructionQueue.Rate.IsEmpty);
+
+			colony.Population[race] = 0;
+			Assert.IsTrue(planet.ConstructionQueue.Rate.IsEmpty);
+		}
+
+		// TODO - test construction of facilities/units/ships
+	}
+}
---
+++ FrEee.Tests/Objects/Vehicles/CloakingTest.cs
@@ -0,0 +1,223 @@
+﻿using FrEee.Interfaces;
+using FrEee.Objects.Abilities;
+using FrEee.Objects.Civilization;
+using FrEee.Objects.Space;
+using FrEee.Objects.Vehicles;
+using FrEee.Modding;
+using FrEee.Extensions;
+using NUnit.Framework;
+using System.Drawing;
+
+namespace FrEee.Tests.Objects.Vehicles
+{
+	/// <summary>
+	/// Tests cloaking of vehicles.
+	/// </summary>
+	public class CloakingTest
+	{
+		/// <summary>
+		/// The ship that is looking for an enemy ship.
+		/// </summary>
+		private static Ship destroyer;
+
+		/// <summary>
+		/// They're controlling the submarine.
+		/// </summary>
+		private static Empire hiders;
+
+		/// <summary>
+		/// They're controlling the destroyer.
+		/// </summary>
+		private static Empire seekers;
+
+		/// <summary>
+		/// The ship that is hiding.
+		/// </summary>
+		private static Ship submarine;
+
+		/// <summary>
+		/// Where the ships are.
+		/// </summary>
+		private static StarSystem sys;
+
+		[OneTimeSetUp]
+		public static void ClassInit()
+		{
+			// initialize galaxy
+			new Galaxy();
+			Mod.Load(null);
+
+			// initialize empires
+			seekers = new Empire();
+			seekers.Name = "Seekers";
+			hiders = new Empire();
+			hiders.Name = "Hiders";
+
+			// initialize ships
+			Assert.IsNotNull(Mod.Current);
+			var dsDesign = new Design<Ship>();
+			dsDesign.BaseName = "TestDestroyer";
+			dsDesign.CreateHull();
+			dsDesign.Owner = seekers;
+			destroyer = dsDesign.Instantiate();
+			destroyer.Owner = seekers;
+			var subDesign = new Design<Ship>();
+			subDesign.BaseName = "TestSubmarine";
+			subDesign.CreateHull();
+			subDesign.Owner = hiders;
+			submarine = subDesign.Instantiate();
+			submarine.Owner = hiders;
+		}
+
+		/// <summary>
+		/// If we have no cloaks, and they have a sensor, they should still be able to see us.
+		/// </summary>
+		[Test]
+		public void AnySensorNoCloakCanSee()
+		{
+			AddSensorAbility(destroyer.Hull, "Foobar", 1);
+			Assert.IsFalse(submarine.IsHiddenFrom(seekers), "Submarine should be visible.");
+		}
+
+		/// <summary>
+		/// If we have high level sight obscuration from our star system, we should be hidden.
+		/// </summary>
+		[Test]
+		public void HighLevelSectorSightObscuration()
+		{
+			AddSensorAbility(destroyer.Hull, "Foobar", 1);
+			AddObscurationAbility(submarine.StarSystem, 2);
+			Assert.IsTrue(submarine.IsHiddenFrom(seekers), "Submarine should be hidden.");
+		}
+
+		/// <summary>
+		/// If we have a cloak, and they have a higher level sensor of the same type, they should be able to see us.
+		/// </summary>
+		[Test]
+		public void HighLevelSensorCanSee()
+		{
+			AddSensorAbility(destroyer.Hull, "Foobar", 2);
+			AddCloakAbility(submarine.Hull, "Foobar", 1);
+			Assert.IsFalse(submarine.IsHiddenFrom(seekers), "Submarine should be visible.");
+		}
+
+		[SetUp]
+		public void Init()
+		{
+			// create star system
+			sys = new StarSystem(0);
+			Galaxy.Current.StarSystemLocations.Add(new ObjectLocation<StarSystem>(sys, new Point()));
+
+			// place ships
+			sys.Place(destroyer, new Point());
+			sys.Place(submarine, new Point());
+		}
+
+		/// <summary>
+		/// If we have a cloak, and they have a lower level sensor of the same type, we should be hidden.
+		/// </summary>
+		[Test]
+		public void LowLevelSensorCantSee()
+		{
+			AddSensorAbility(destroyer.Hull, "Foobar", 1);
+			AddCloakAbility(submarine.Hull, "Foobar", 2);
+			Assert.IsTrue(submarine.IsHiddenFrom(seekers), "Submarine should be hidden.");
+		}
+
+		/// <summary>
+		/// If we have a cloak, and they have a different type of sensor that we don't have a cloak in, they should be able to see us.
+		/// </summary>
+		[Test]
+		public void MismatchedSensorTypeCanSee()
+		{
+			AddSensorAbility(destroyer.Hull, "Narf", 1);
+			AddCloakAbility(submarine.Hull, "Foobar", 999);
+			Assert.IsFalse(submarine.IsHiddenFrom(seekers), "Submarine should be visible.");
+		}
+
+		/// <summary>
+		/// If everyone is in a nebula with a system cloaking ability, the enemy ship should be hidden.
+		/// </summary>
+		[Test]
+		public void NebulaCantSee()
+		{
+			AddSensorAbility(destroyer.Hull, "Foobar", 1);
+			sys.Abilities.Add(new Ability(sys, Mod.Current.AbilityRules.FindByName("System - Sight Obscuration"), null, 999));
+			Assert.IsTrue(submarine.IsHiddenFrom(seekers), "Submarine should be hidden.");
+		}
+
+		/// <summary>
+		/// If we have a cloak, and they have no sensors, they should not be able to see us.
+		/// </summary>
+		[Test]
+		public void NoSensorAnyCloakCantSee()
+		{
+			AddCloakAbility(submarine.Hull, "Foobar", 1);
+			Assert.IsTrue(submarine.IsHiddenFrom(seekers), "Submarine should be hidden.");
+		}
+
+		/// <summary>
+		/// If we have no cloaks, and they have no sensors, they should still be able to see us.
+		/// </summary>
+		[Test]
+		public void NoSensorNoCloakCanSee()
+		{
+			// by default the ships will have no abilities, so let's just test
+			Assert.IsFalse(submarine.IsHiddenFrom(seekers), "Submarine should be visible.");
+		}
+
+		/// <summary>
+		/// If we have a cloak, and they have the same level sensor of the same type, they should be able to see us.
+		/// </summary>
+		[Test]
+		public void SameLevelSensorCanSee()
+		{
+			AddSensorAbility(destroyer.Hull, "Foobar", 1);
+			AddCloakAbility(submarine.Hull, "Foobar", 1);
+			Assert.IsFalse(submarine.IsHiddenFrom(seekers), "Submarine should be visible.");
+		}
+
+		/// <summary>
+		/// If the enemy is in a storm with a sector cloaking ability, the enemy ship should be hidden.
+		/// </summary>
+		[Test]
+		public void StormCantSee()
+		{
+			AddSensorAbility(destroyer.Hull, "Foobar", 1);
+			var storm = new Storm();
+			storm.Abilities.Add(new Ability(sys, Mod.Current.AbilityRules.FindByName("Sector - Sight Obscuration"), null, 999));
+			sys.Place(storm, new Point());
+			Assert.IsTrue(submarine.IsHiddenFrom(seekers), "Submarine should be hidden.");
+		}
+
+		// TODO - same/low level sight obscuration from star system
+
+		// TODO - sight obscuration from sector
+
+		private void AddCloakAbility(IHull hull, string sightType, int level)
+		{
+			var a = new Ability(hull);
+			hull.Abilities.Add(a);
+			a.Rule = Mod.Current.AbilityRules.FindByName("Cloak Level");
+			a.Values.Add(sightType);
+			a.Values.Add(level.ToString());
+		}
+
+		private void AddObscurationAbility(IAbilityContainer obj, int level)
+		{
+			var a = new Ability(obj);
+			obj.Abilities.Add(a);
+			a.Rule = Mod.Current.AbilityRules.FindByName("Sector - Sight Obscuration");
+			a.Values.Add(level.ToString());
+		}
+
+		private void AddSensorAbility(IHull hull, string sightType, int level)
+		{
+			var a = new Ability(hull);
+			hull.Abilities.Add(a);
+			a.Rule = Mod.Current.AbilityRules.FindByName("Sensor Level");
+			a.Values.Add(sightType);
+			a.Values.Add(level.ToString());
+		}
+	}
+}
