f24967257f8ff14b13b5bc269f1ded23ec15a1d9
Ed Kolis <edkolis@gmail.com>
2024-02-02 23:00:42 -0500

    Create a web host project that does nothing

---
+++ FrEee.WebHost/Program.cs
@@ -0,0 +1,51 @@
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.Identity.Abstractions;
+using Microsoft.Identity.Web;
+using Microsoft.Identity.Web.Resource;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+	.AddMicrosoftIdentityWebApi(builder.Configuration.GetSection("AzureAd"));
+builder.Services.AddAuthorization();
+
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+
+app.UseHttpsRedirection();
+
+app.UseAuthentication();
+app.UseAuthorization();
+
+var scopeRequiredByApi = app.Configuration["AzureAd:Scopes"] ?? "";
+var summaries = new[]
+{
+	"Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+};
+
+app.MapGet("/weatherforecast", (HttpContext httpContext) =>
+{
+	httpContext.VerifyUserHasAnyAcceptedScope(scopeRequiredByApi);
+
+	var forecast = Enumerable.Range(1, 5).Select(index =>
+		new WeatherForecast
+		(
+			DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+			Random.Shared.Next(-20, 55),
+			summaries[Random.Shared.Next(summaries.Length)]
+		))
+		.ToArray();
+	return forecast;
+})
+.RequireAuthorization();
+
+app.Run();
+
+internal record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
+{
+	public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+}
