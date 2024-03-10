07b7e7babfe22c7f4533134a397794c998a9e578
Ed Kolis <edkolis@gmail.com>
2024-03-09 19:23:44 -0500

    Actually render the Blazor view in the resource display

--- FrEee.UI.WinForms/Controls/ResourceDisplay.Designer.cs
+++ FrEee.UI.WinForms/Controls/ResourceDisplay.Designer.cs
@@ -44,6 +44,7 @@ partial class ResourceDisplay
 		// 
 		// ResourceDisplay
 		// 
+		Controls.Add(blazorView);
 		this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
 		this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
 		this.BackColor = System.Drawing.Color.Black;
