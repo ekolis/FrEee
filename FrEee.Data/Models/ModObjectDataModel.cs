d54fd52a7d0140826cdf2d2c1356a5861defa49b
Ed Kolis <edkolis@gmail.com>
2024-05-27 16:46:08 -0400

    Data model base classes for referrables and mod objects

---
+++ FrEee.Data/Models/ModObjectDataModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FrEee.Data.Models
+{
+	public class ModObjectDataModel(string modID)
+		: IDataModel
+	{
+		/// <summary>
+		/// The ID of the mod object.
+		/// </summary>
+		public string ModID { get; set; } = modID;
+	}
+}
---
+++ FrEee.Data/Models/ReferrableDataModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FrEee.Data.Models
+{
+
+	public class ReferrableDataModel(long id)
+		: IDataModel
+	{
+		/// <summary>
+		/// The ID of the referrable.
+		/// </summary>
+		public long ID { get; set; } = id;
+	}
+}
