5f98bd25077a4bae5523f3e24561f8fc81f53b19
Ed Kolis <edkolis@gmail.com>
2024-06-09 13:20:39 -0400

    Delete duplicate class

--- FrEee.Core/Utility/ReferenceSet.cs
+++ FrEee.Core/Utility/ReferenceSet.cs
@@ -8,21 +8,6 @@ using FrEee.Extensions;
 
 namespace FrEee.Serialization;
 
-[Serializable]
-public class ModReferenceSet<T> : ReferenceSet<ModReference<T>, T>
-		where T : IModObject
-{
-	public ModReferenceSet()
-		: base()
-	{
-	}
-
-	public ModReferenceSet(IEnumerable<T> objs)
-		: base(objs)
-	{
-	}
-}
-
 /// <summary>
 /// A set of references.
 /// </summary>
