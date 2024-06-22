21263b565a6821b7a414d08b916245fb582c5d96
Ed Kolis <edkolis@gmail.com>
2024-06-22 15:55:36 -0400

    Add scopes to abilities. They don't do anything yet, thoguh.

---
+++ FrEee.Core.Domain/Ecs/Abilities/Utility/OwnershipScopes.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FrEee.Ecs.Abilities.Utility
+{
+	/// <summary>
+	/// Scope of an ability in terms of the ownership of the entities it can affect.
+	/// </summary>
+	[Flags]
+	public enum OwnershipScopes
+	{
+		Self = 0x1,
+		Allies = 0x2,
+		Neutral = 0x4,
+		Enemies = 0x8,
+		Unowned = 0x10,
+
+		Default = Self,
+		Friends = Self | Allies,
+		Hostiles = Neutral | Enemies,
+		Owned = Friends | Hostiles,
+		All = Owned | Unowned,
+	}
+}
---
+++ FrEee.Core.Domain/Ecs/Abilities/Utility/SpatialScope.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FrEee.Ecs.Abilities.Utility
+{
+	/// <summary>
+	/// Scope of an ability in terms of the locations of the entities it can affect.
+	/// </summary>
+	public enum SpatialScope
+	{
+		Entity = 1,
+		SpaceObject = 2,
+		Sector = 3,
+		StarSystem = 4,
+		Galaxy = 5,
+
+		Default = SpaceObject,
+	}
+}
