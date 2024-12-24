e51720754daf94d7bf3d1c65e62d23157602817b
Ed Kolis <edkolis@gmail.com>
2024-12-24 14:29:07 -0500

    Add all-purpose translation methods to DataTranslators

--- FrEee.Data/DataTranslators.cs
+++ FrEee.Data/DataTranslators.cs
@@ -25,4 +25,42 @@ public class DataTranslators
 	private IEnumerable<IDataTranslator> all;
 
 	public static IEnumerable<IDataTranslator> All => instance.all;
+
+	/// <summary>
+	/// Attempts to translate an object to a data representation.
+	/// </summary>
+	/// <param name="obj"></param>
+	/// <returns>The data representation, or the original object if there is no available translator for the object type.</returns>
+	public static object ToData(object obj)
+	{
+		var translator = All.SingleOrDefault(q => q.ObjectType == obj.GetType());
+		if (translator is null)
+		{
+			// can't translate, no translator found
+			return obj;
+		}
+		else
+		{
+			return translator.ToData(obj);
+		}
+	}
+
+	/// <summary>
+	/// Attempts to translate data to an object.
+	/// </summary>
+	/// <param name="obj"></param>
+	/// <returns>The object, or the original data if there is no available translator for the data type.</returns>
+	public static object FromData(object obj)
+	{
+		var translator = All.SingleOrDefault(q => q.DataType == obj.GetType());
+		if (translator is null)
+		{
+			// can't translate, no translator found
+			return obj;
+		}
+		else
+		{
+			return translator.FromData(obj);
+		}
+	}
 }
