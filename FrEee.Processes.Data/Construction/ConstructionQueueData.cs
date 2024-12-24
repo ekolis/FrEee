187795f3a01b396315aee8d0995323df0620cb7a
Ed Kolis <edkolis@gmail.com>
2024-12-24 17:31:10 -0500

    Add data object types for ConstructionQueue

---
+++ FrEee.Processes.Data/Construction/ConstructionQueueData.cs
@@ -0,0 +1,45 @@
+﻿using FrEee.Objects.Space;
+using FrEee.Objects.Technology;
+using FrEee.Modding;
+using FrEee.Utility;
+using FrEee.Serialization;
+using FrEee.Extensions;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using FrEee.Objects.Civilization.CargoStorage;
+using FrEee.Objects.Civilization.Orders;
+using FrEee.Objects.GameState;
+using FrEee.Modding.Templates;
+using FrEee.Modding.Abilities;
+using FrEee.Vehicles;
+using FrEee.Vehicles.Types;
+using FrEee.Objects.Civilization;
+
+namespace FrEee.Processes.Construction;
+
+/// <summary>
+/// Data to represent a <see cref="ConstructionQueue"/>.
+/// </summary>
+public record ConstructionQueueData
+{
+	public bool IsOnHold { get; set; }
+
+	public bool IsOnRepeat { get; set; }
+
+	public required GameReference<IConstructor> Container { get; set; }
+
+	public long ID { get; set; }
+
+	public bool IsConstructionDelayed { get; set; }
+
+	public bool IsDisposed { get; set; }
+
+	// not a GameReferenceList, these orders are contained within the construction queue
+	public IList<IConstructionOrder> Orders { get; set; } = [];
+
+	public double Timestamp { get; set; }
+
+	public ResourceQuantity UnspentRate { get; set; } = [];
+}
---
+++ FrEee.Processes.Data/Construction/ConstructionQueueDataTranslator.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Composition;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FrEee.Data;
+using FrEee.Extensions;
+using Microsoft.Scripting.Utils;
+
+namespace FrEee.Processes.Construction;
+
+[Export(typeof(IDataTranslator))]
+public class ConstructionQueueDataTranslator
+	: DataTranslator<ConstructionQueue, ConstructionQueueData>
+{
+	public override ConstructionQueue FromData(ConstructionQueueData data)
+	{
+		ConstructionQueue obj = new(data.Container.Value)
+		{
+			ID = data.ID,
+			IsConstructionDelayed = data.IsConstructionDelayed,
+			IsDisposed = data.IsDisposed,
+			IsOnHold = data.IsOnHold,
+			IsOnRepeat = data.IsOnRepeat,
+			Timestamp = data.Timestamp,
+			UnspentRate = data.UnspentRate,
+		};
+		obj.Orders.AddRange(data.Orders);
+		return obj;
+	}
+
+	public override ConstructionQueueData ToData(ConstructionQueue obj)
+	{
+		return new()
+		{
+			Container = obj.Container.ReferViaGalaxy(),
+			ID = obj.ID,
+			IsConstructionDelayed = obj.IsConstructionDelayed,
+			IsDisposed = obj.IsDisposed,
+			IsOnHold = obj.IsOnHold,
+			IsOnRepeat = obj.IsOnRepeat,
+			Orders = obj.Orders,
+			Timestamp = obj.Timestamp,
+			UnspentRate = obj.UnspentRate,
+		};
+	}
+}
