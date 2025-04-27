225745fcc286e22f561f345f27111d2f6bf01747
Ed Kolis <edkolis@gmail.com>
2025-04-27 13:39:55 -0400

    Convert ModReference to a record type

--- FrEee.Core.Domain/Modding/ModReference.cs
+++ FrEee.Core.Domain/Modding/ModReference.cs
@@ -14,30 +14,15 @@ namespace FrEee.Modding;
 /// It is not necessary to use ModReference when referring to mod objects from other mod objects.
 /// Only mod objects can be referenced via mod references.
 /// </summary>
-/// <typeparam name="T"></typeparam>
+/// <param name="ID">The ID of the <see cref="IModObject"/> being referenced.</param>
+/// <typeparam name="T">The type of the <see cref="IModObject"/> being referenced.</typeparam>
 [Serializable]
-public class ModReference<T> : IReference<string, T> where T : IModObject
+public record ModReference<T>(string ID)
+	: IReference<string, T>
+	where T : IModObject
 {
-	public ModReference()
-	{
-		cache = new ClientSideCache<T>(() =>
-		{
-			if (string.IsNullOrWhiteSpace(ID))
-				return default(T);
-			var obj = Mod.Current.Find<T>(ID);
-			if (obj == null)
-				obj = Mod.Current.Objects.OfType<T>().FirstOrDefault(q => q.Name == ID);
-			if (obj == null)
-				return default(T);
-			if (obj.IsDisposed)
-				return default(T);
-			return obj;
-		})
-		{ IsServerSideCacheEnabled = true };
-	}
-
-	public ModReference(T t)
-		: this()
+	public ModReference(T? t)
+		: this(t?.ModID)
 	{
 		var mobj = (IModObject)t;
 		if (Mod.Current == null)
@@ -52,17 +37,6 @@ public class ModReference<T> : IReference<string, T> where T : IModObject
 			throw new ArgumentException("{0} does not exist in the current mod so it cannot be referenced.".F(t));
 	}
 
-	public ModReference(string id)
-		: this()
-	{
-		if (Mod.Current == null)
-			throw new ReferenceException<int, T>("Can't create a reference to an IModObject without a mod.");
-		else if (Mod.Current.Find<T>(id) is T)
-			ID = id;
-		else
-			throw new Exception("Object with ID " + id + " is not a " + typeof(T) + ".");
-	}
-
 	/// <summary>
 	/// Does the reference have a valid value?
 	/// </summary>
@@ -75,8 +49,6 @@ public class ModReference<T> : IReference<string, T> where T : IModObject
 		}
 	}
 
-	public string ID { get; private set; }
-
 	/// <summary>
 	/// Resolves the reference.
 	/// </summary>
@@ -85,20 +57,15 @@ public class ModReference<T> : IReference<string, T> where T : IModObject
 	{
 		get
 		{
-			var val = cache.Value;
-			if (val == null)
-				return default(T);
-			return val;
+			return (T)Mod.Current.Objects.FindByModID(ID);
 		}
 	}
 
-	private ClientSideCache<T> cache;
-
-	public static implicit operator ModReference<T>(T t)
+	public static implicit operator ModReference<T>?(T? t)
 	{
-		if (t == null)
+		if (t is null)
 			return null;
-		if (t.ModID == null)
+		if (t.ModID is null)
 		{
 			// HACK - why are my items not registered in the mod? does it have something to do with formulas and leveled templates?
 			Mod.Current.AssignID(t, Mod.Current.Objects.Select(q => q.ModID).ToList());
@@ -106,37 +73,29 @@ public class ModReference<T> : IReference<string, T> where T : IModObject
 		return new ModReference<T>(t);
 	}
 
-	public static implicit operator T(ModReference<T> r)
+	public static implicit operator T?(ModReference<T>? r)
 	{
-		if (r == null)
-			return default(T);
+		if (r is null)
+			return default;
 		return r.Value;
 	}
 
-	public override bool Equals(object? obj)
+	public ModReference<T> ReplaceClientIDs(IDictionary<long, long> idmap, ISet<IPromotable>? done = null)
 	{
-		var mref = obj as ModReference<T>;
-		if (mref == null)
-			return false;
-		return ID == mref.ID;
-	}
-
-	public override int GetHashCode()
-	{
-		return ID == null || ID == "" ? 0 : ID.GetHashCode();
-	}
-
-	public IPromotable ReplaceClientIDs(IDictionary<long, long> idmap, ISet<IPromotable> done = null)
-	{
-		if (done == null)
-			done = new HashSet<IPromotable>();
+		var result = this;
+		done ??= new HashSet<IPromotable>();
 		if (!done.Contains(this))
 		{
 			done.Add(this);
-			if (HasValue && Value is IPromotable)
-				((IPromotable)Value).ReplaceClientIDs(idmap, done);
+			if (HasValue && Value is IPromotable p)
+				p.ReplaceClientIDs(idmap, done);
 		}
-		return this;
+		return result;
+	}
+
+	IPromotable IPromotable.ReplaceClientIDs(IDictionary<long, long> idmap, ISet<IPromotable> done)
+	{
+		return ReplaceClientIDs(idmap, done);
 	}
 
 	public override string ToString()
