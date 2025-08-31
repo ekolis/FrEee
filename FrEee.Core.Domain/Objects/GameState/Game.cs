ea54a8dff971bc98e51cc110913d0e64504d7256
Ed Kolis <edkolis@gmail.com>
2025-08-31 17:55:27 -0400

    Cache the filtered referrables list

--- FrEee.Core.Domain/Objects/GameState/Game.cs
+++ FrEee.Core.Domain/Objects/GameState/Game.cs
@@ -204,7 +204,7 @@ public class Game
 			.. Designs.SelectMany(emp => emp.ReferrableTree()),
 			.. NewReferrables
 		];
-		referrables = result.Distinct().ExceptNull();
+		referrables = result.Distinct().ExceptNull().ToArray();
 	}
 
 	/// <summary>
