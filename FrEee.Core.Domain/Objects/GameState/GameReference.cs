36ec6c7bec12145181e68f4a3dbf0ede680f8a8a
Ed Kolis <edkolis@gmail.com>
2025-08-31 17:45:30 -0400

    Look up references to empires and designs directly rather than needing the whole referrables list

--- FrEee.Core.Domain/Objects/GameState/GameReference.cs
+++ FrEee.Core.Domain/Objects/GameState/GameReference.cs
@@ -2,6 +2,9 @@ using FrEee.Extensions;
 using System;
 using System.Collections.Generic;
 using FrEee.Utility;
+using FrEee.Vehicles;
+using System.Linq;
+using FrEee.Objects.Civilization;
 
 namespace FrEee.Objects.GameState;
 
@@ -47,7 +50,19 @@ public record GameReference<T>(long ID)
 	{
 		get
 		{
-			return (T)Game.Current.GetReferrable(ID);
+			if (typeof(T).IsAssignableTo(typeof(Empire)))
+			{
+				return (T)(object)Game.Current.Empires.SingleOrDefault(q => q.ID == ID);
+			}
+			if (typeof(T).IsAssignableTo(typeof(IDesign)))
+			{
+				return (T)Game.Current.Designs.SingleOrDefault(q => q.ID == ID);
+			}
+			else
+			{
+				// general referrables with no specific lookup
+				return (T)Game.Current.GetReferrable(ID);
+			}
 		}
 	}
 
