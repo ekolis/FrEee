e90b192aa1488d4d3635a2e9b166e0600695f163
Ed Kolis <edkolis@gmail.com>
2025-09-21 12:23:17 -0400

    Fix research progress bar always showing a minimum of 3 turns to completion graphically, even when it says 1 or 2 textually

--- FrEee.UI.Blazor/Views/ProgressBarViewModel.cs
+++ FrEee.UI.Blazor/Views/ProgressBarViewModel.cs
@@ -64,5 +64,40 @@ namespace FrEee.UI.Blazor.Views
 		}
 
 		public ProgressDisplayType ProgressDisplayType { get; set; } = ProgressDisplayType.Percentage;
+
+		/// <summary>
+		/// Calculates the per-turn progress percentage to show on the bar after a certain number of turns.
+		/// </summary>
+		/// <param name="turnsAhead">The number of turns to look ahead.</param>
+		/// <returns>
+		/// The progress percentage achieved on the specified turn (not incremental).
+		/// Capped so that the total progress never exceeds 100%.
+		/// </returns>
+		public double GetProgressPercentage(int turnsAhead)
+		{
+			// calculate starting point for current turn
+			var currentValue = Progress.Value;
+			var totalValue = currentValue;
+
+			// calculate progress for future turns
+			for (var turn = 1; turn <= turnsAhead; turn++)
+			{
+				// apply incremental progress
+				currentValue = Increment;
+				totalValue += currentValue;
+
+				// cap at maximum
+				if (totalValue > Maximum)
+				{
+					currentValue -= totalValue - Maximum;
+					totalValue = Maximum;
+				}
+			}
+
+			// calculate and return percentage
+			var max = Maximum > 0 ? Maximum : 1; // avoid divide by zero
+			var currentPercentage = currentValue * 100d / max; // allow fractional percentages to prevent rounding error
+			return currentPercentage;
+		}
 	}
 }
